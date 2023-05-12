56a67bff2eb3d4f4ba702c86820e4c33a761bcc3
Shahules786 <Shahules786@gmail.com>
2023-05-13 01:25:48 +0530

    add relative import

--- belar/metrics/__init__.py
+++ belar/metrics/__init__.py
@@ -1,3 +1,4 @@
 from belar.metrics.base import Evaluation, Metric
 from belar.metrics.simple import *
 from belar.metrics.similarity import SBERTScore
+from belar.metrics.factual import EntailmentScore
