790651e21e425e4ce8203c90336e4beafa6741e5
Jithin James <jjmachan@pop-os.localdomain>
2023-05-13 02:32:05 +0530

    basic batching and variable ground_truth support

--- belar/metrics/similarity.py
+++ belar/metrics/similarity.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import typing as t
 from dataclasses import dataclass
+
 import numpy as np
 from numpy.linalg import norm
 from sentence_transformers import SentenceTransformer
