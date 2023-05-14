40af06709629c4536274a12da7a532340700dd55
Jithin James <jjmachan@pop-os.localdomain>
2023-05-15 00:41:25 +0530

    added __version__ to package

---
+++ ragas/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
