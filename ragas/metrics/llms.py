2868bc289b108ca9459d61560ed24b503bf59c06
Jithin James <jamesjithin97@gmail.com>
2023-06-07 20:11:21 +0530

    code for `ragas.metric` (#34)
    * added code from experiment
    
    * added basic modules for metrics
    
    * reformated the code
    
    * Revert "reformated the code"
    
    This reverts commit 6cb7cde5bb3b214ae7993ca5e8373e0064c0ed68.
    
    * basic implementation of ragas
    
    * qgen.py
    
    * moved files arround
    
    * pyarrow benchmarks
    
    * moved to a simpler abstraction
    
    * added qgen_context
    
    * consistant naming of Factuality Answer-relevancy and context_relevancy
    
    * fix factual
    
    * minor fixes
    
    * corrected nli score
    
    * fixes

---
+++ ragas/metrics/llms.py
@@ -0,0 +1,52 @@
+import logging
+import os
+
+import backoff
+import openai
+from openai.error import RateLimitError
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+# TODO better way of logging backoffs
+logging.getLogger("backoff").addHandler(logging.StreamHandler())
+
+
+# each of these calls have to check for
+# https://platform.openai.com/docs/guides/error-codes/api-errors
+# and handle it gracefully
+@backoff.on_exception(backoff.expo, RateLimitError, max_tries=5)
+def openai_completion(prompts: list[str], **kwargs):
+    """
+    TODOs
+
+    - what happens when backoff fails?
+    """
+    response = openai.Completion.create(
+        model=kwargs.get("model", "text-davinci-003"),
+        prompt=prompts,
+        temperature=kwargs.get("temperature", 0),
+        top_p=kwargs.get("top_p", 1),
+        frequency_penalty=kwargs.get("frequency_penalty", 0.0),
+        presence_penalty=kwargs.get("presence_penalty", 0.0),
+        max_tokens=kwargs.get("max_tokens", 500),
+        logprobs=kwargs.get("logprobs", 1),
+        n=kwargs.get("n", 1),
+    )
+
+    return response
+
+
+# TODO: make this work
+def openai_completion_async(prompts: list[str], **kwargs):
+    response = openai.Completion.acreate(
+        model=kwargs.get("model", "text-davinci-003"),
+        prompt=prompts,
+        temperature=kwargs.get("temperature", 0),
+        top_p=kwargs.get("top_p", 1),
+        frequency_penalty=kwargs.get("frequency_penalty", 0.0),
+        presence_penalty=kwargs.get("presence_penalty", 0.0),
+        max_tokens=kwargs.get("max_tokens", 500),
+        logprobs=kwargs.get("logprobs", 1),
+        n=kwargs.get("n", 1),
+    )
+    return response
