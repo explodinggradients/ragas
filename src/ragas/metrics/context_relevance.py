f5493747f239e6ce9c2530eb132ccadba3a0f848
Shahules786 <Shahules786@gmail.com>
2023-07-13 10:53:45 +0530

    format code

--- src/ragas/metrics/context_relevance.py
+++ src/ragas/metrics/context_relevance.py
@@ -2,18 +2,16 @@ from __future__ import annotations
 
 import typing as t
 from dataclasses import dataclass
-
-from datasets import Dataset
-from pandas import DataFrame
-
-from ragas.metrics.base import Metric
+from itertools import combinations, product
+from typing import List
 
 import numpy as np
-from typing import List
-from itertools import combinations, product
+from datasets import Dataset
+from pandas import DataFrame
 from sentence_transformers import CrossEncoder
-from ragas.metrics.llms import openai_completion
 
+from ragas.metrics.base import Metric
+from ragas.metrics.llms import openai_completion
 
 CONTEXT_RELEVANCE = """
 Task: Candidate sentence extraction.
@@ -32,40 +30,38 @@ sentences:Scott Derrickson (born July 16, 1966) is an American director, screenw
 question:{}
 context:\n{}
 sentences:"""
-        
-def sent_tokenize(sent):
-    return [s[:-1] if  s.endswith('.') else s for s in sent.strip().split('. ')]
+
+
+def sent_tokenize(sent: str):
+    return [s[:-1] if s.endswith(".") else s for s in sent.strip().split(". ")]
+
 
 class SentenceAgreement:
-    
-    def __init__(self, model_name="cross-encoder/stsb-TinyBERT-L-4", metric="bert_score"):
-        
+    def __init__(
+        self, model_name="cross-encoder/stsb-TinyBERT-L-4", metric="bert_score"
+    ):
         self.metric = metric
         self.cross_encoder = CrossEncoder(model_name)
 
-        
-    def bert_score(self, para1, para2):
-        
+    def bert_score(self, para1: str, para2: str):
         sentences1, sentences2 = sent_tokenize(para1), sent_tokenize(para2)
         scores = self.cross_encoder.predict(list(product(sentences1, sentences2)))
         scores = scores.reshape(len(sentences1), len(sentences2))
         return scores.max(axis=1).mean()
 
     @staticmethod
-    def jaccard_score(para1, para2):
-        
+    def jaccard_score(para1: str, para2: str):
         sentences1, sentences2 = sent_tokenize(para1), sent_tokenize(para2)
         intersect = len(np.intersect1d(sentences1, sentences2))
         union = len(np.union1d(sentences1, sentences2))
-        return intersect/union
-    
-    def evaluate(self,answers:List[List[str]]):
-        
+        return intersect / union
+
+    def evaluate(self, answers: List[List[str]]):
         """
         eval nC2 combinations
         """
         scores = []
-        groups = combinations(answers,2)
+        groups = combinations(answers, 2)
         for group in groups:
             if self.metric == "jaccard":
                 score = self.jaccard_score(*group)
@@ -73,53 +69,60 @@ class SentenceAgreement:
                 score = self.bert_score(*group)
             scores.append(score)
         return np.mean(scores)
-            
+
 
 @dataclass
 class ContextRelevancy(Metric):
     name: str = "context_relavency"
+    batch_size: int = 15
+    agreement_metric: str = "bert_score"
+    strictness: int = 2
+    model_name: str = "cross-encoder/stsb-TinyBERT-L-4"
 
-    def init_model(self: t.Self, 
-                   strictness=1,
-                   model_name="cross-encoder/stsb-TinyBERT-L-4",
-                   agreement_metric="bert_score"):
-        
-        self.strictness = strictness
-        self.agreement_metric = agreement_metric
-        self.sent_agreement = SentenceAgreement(model_name=model_name, metric=agreement_metric)
+    def init_model(self: t.Self):
+        self.sent_agreement = SentenceAgreement(
+            model_name=self.model_name, metric=self.agreement_metric
+        )
 
     def score(self: t.Self, dataset: Dataset) -> Dataset:
         """
         dataset: Dataset[question: list[str], contexts: list[list[str]]]
         """
         prompts = []
-        question, context = dataset["question"], dataset["contexts"]
-        for q, c in zip(question, context):
+        questions, contexts = dataset["question"], dataset["contexts"]
+        for q, c in zip(questions, contexts):
             prompt = CONTEXT_RELEVANCE.format(q, "\n".join(c))
             prompts.append(prompt)
-        
+
         responses = openai_completion(prompts, n=self.strictness)
-        
+
         prev = 0
         outputs = []
-        for i in range(self.strictness,len(responses["choices"]), self.strictness):
-            output = [responses["choices"][idx]["text"].strip() for idx in range(prev, i)]
+        for i in range(self.strictness, len(responses["choices"]) + 1, self.strictness):
+            output = [
+                responses["choices"][idx]["text"].strip() for idx in range(prev, i)
+            ]
             outputs.append(output)
             prev = i
-        
+
         scores = []
-        for context, n_output in zip(question, outputs):
+        for context, n_output in zip(contexts, outputs):
+            context = "\n".join(context)
             overlap_scores = []
             context_sents = sent_tokenize(context)
             for output in n_output:
-                indices = [context.find(sent) for sent in sent_tokenize(output) if context.find(sent)!=-1]
+                indices = [
+                    context.find(sent)
+                    for sent in sent_tokenize(output)
+                    if context.find(sent) != -1
+                ]
                 overlap_scores.append(len(indices) / len(context_sents))
             if self.strictness > 1:
                 agr_score = self.sent_agreement.evaluate(n_output)
             else:
-                agr_score = 1 
+                agr_score = 1
             scores.append(agr_score * np.mean(overlap_scores))
-        
+
         dataset = dataset.add_column(self.name, scores)  # type: ignore
         df = dataset.to_pandas()
         assert isinstance(df, DataFrame)
