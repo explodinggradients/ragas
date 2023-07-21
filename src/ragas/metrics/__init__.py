91d67250902b7a728854ad169e470535cf1b3b79
Shahules786 <Shahules786@gmail.com>
2023-07-21 16:48:05 +0530

    added aspect critique

--- src/ragas/metrics/__init__.py
+++ src/ragas/metrics/__init__.py
@@ -1,6 +1,7 @@
 from ragas.metrics.answer_relevance import AnswerRelevancy, answer_relevancy
 from ragas.metrics.context_relevance import ContextRelevancy, context_relevancy
 from ragas.metrics.faithfulnes import Faithfulness, faithfulness
+from ragas.metrics.critique import AspectCritique
 
 __all__ = [
     "Faithfulness",
@@ -9,4 +10,5 @@ __all__ = [
     "answer_relevancy",
     "ContextRelevancy",
     "context_relevancy",
+    "AspectCritique",
 ]
