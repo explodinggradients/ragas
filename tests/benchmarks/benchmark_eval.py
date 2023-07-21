70862f92d3c1e2a6c1283390630778fa7ca03cc8
Shahules786 <Shahules786@gmail.com>
2023-07-21 16:48:19 +0530

    added new metrics to tests

--- tests/benchmarks/benchmark_eval.py
+++ tests/benchmarks/benchmark_eval.py
@@ -3,6 +3,7 @@ from torch.cuda import is_available
 
 from ragas import evaluate
 from ragas.metrics import answer_relevancy, context_relevancy, faithfulness
+from ragas.metrics.critique import harmfulness
 
 DEVICE = "cuda" if is_available() else "cpu"
 
@@ -12,6 +13,6 @@ ds = load_dataset("explodinggradients/fiqa", "ragas_eval")["baseline"]
 if __name__ == "__main__":
     result = evaluate(
         ds,
-        metrics=[answer_relevancy, context_relevancy, faithfulness],
+        metrics=[answer_relevancy, context_relevancy, faithfulness, harmfulness],
     )
     print(result)
