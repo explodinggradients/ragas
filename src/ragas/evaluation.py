aae79499b9ea4b80e3ce4ee97f95eb9f997969c5
jjmachan <jamesjithin97@gmail.com>
2023-08-13 00:19:39 +0530

    ignore type

--- src/ragas/evaluation.py
+++ src/ragas/evaluation.py
@@ -117,7 +117,7 @@ class Result(dict):
 
         # harmonic mean of all the scores we have
         if len(values) > 1:
-            reciprocal_sum = np.sum(1.0 / np.array(values))
+            reciprocal_sum = np.sum(1.0 / np.array(values))  # type: ignore
             self["ragas_score"] = len(values) / reciprocal_sum
 
     def to_pandas(self, batch_size: int | None = None, batched: bool = False):
