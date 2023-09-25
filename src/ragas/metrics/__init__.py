a25c471569f9734f368b6df96471b3ce258776bf
jjmachan <jamesjithin97@gmail.com>
2023-09-25 12:46:33 +0530

    fix llamaindex issues

--- src/ragas/metrics/__init__.py
+++ src/ragas/metrics/__init__.py
@@ -2,7 +2,9 @@ from ragas.metrics.answer_relevance import AnswerRelevancy, answer_relevancy
 from ragas.metrics.context_recall import ContextRecall, context_recall
 from ragas.metrics.context_relevance import ContextRelevancy, context_relevancy
 from ragas.metrics.critique import AspectCritique
-from ragas.metrics.faithfulnes import Faithfulness, faithfulness
+from ragas.metrics.faithfulness import Faithfulness, faithfulness
+
+DEFAULT_METRICS = [answer_relevancy, context_relevancy, faithfulness, context_recall]
 
 __all__ = [
     "Faithfulness",
