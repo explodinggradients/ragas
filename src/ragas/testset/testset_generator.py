12aa0f7954eebc37b486df1b8db19b3d4593e8d0
Shahules786 <Shahules786@gmail.com>
2023-09-25 15:46:51 +0530

    bug fixes and type hints

--- src/ragas/testset/testset_generator.py
+++ src/ragas/testset/testset_generator.py
@@ -1,21 +1,22 @@
 import typing as t
 from collections import defaultdict, namedtuple
+from dataclasses import dataclass
 
 import numpy as np
 import numpy.testing as npt
 import pandas as pd
-from attr import dataclass
 from langchain.chat_models import ChatOpenAI
 from langchain.chat_models.base import BaseChatModel
 from langchain.embeddings.base import Embeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms.base import BaseLLM
 from langchain.prompts import ChatPromptTemplate
-from llama_index import OpenAIEmbedding
 from llama_index.indices.query.embedding_utils import get_top_k_embeddings
 from llama_index.node_parser.simple import SimpleNodeParser
-from llama_index.readers.schema.base import Document
-from llama_index.schema import TextNode
+from llama_index.readers.schema import Document
+from llama_index.schema import BaseNode
 from numpy.random import default_rng
+from tqdm import tqdm
 
 from ragas.metrics.llms import generate
 from ragas.testset.prompts import (
@@ -43,6 +44,40 @@ question_deep_map = {
     "conditional": "_condition_question",
 }
 
+Testdata_tuple = namedtuple(
+    "Testdata_tuple", ["question", "context", "answer", "question_type"]
+)
+
+
+@dataclass
+class TestDataset:
+    """
+    TestDataset class
+    """
+
+    test_data: t.List[Testdata_tuple]
+
+    def to_pandas(self) -> pd.DataFrame:
+        data_samples = []
+        for data in self.test_data:
+            is_conv = len(data.context) > 1
+            question_type = data.question_type
+            data = [
+                {
+                    "question": qstn,
+                    "context": ctx,
+                    "answer": ans,
+                    "question_type": question_type,
+                    "episode_done": True,
+                }
+                for qstn, ctx, ans in zip(data.question, data.context, data.answer)
+            ]
+            if is_conv:
+                data[0].update({"episode_done": False})
+            data_samples.extend(data)
+
+        return pd.DataFrame.from_records(data_samples)
+
 
 class TestsetGenerator:
 
@@ -51,6 +86,17 @@ class TestsetGenerator:
 
     Attributes
     ----------
+    generator_llm: BaseLLM | BaseChatModel
+        LLM used for all the generator operations in the TestGeneration paradigm.
+    critique_llm: BaseLLM | BaseChatModel
+        LLM used for all the filtering and scoring operations in TestGeneration
+        paradigm.
+    embeddings_model: Embeddings
+        Embeddings used for vectorizing nodes when required.
+    chat_qa: float
+        Determines the fraction of conversational questions the resulting test set.
+    chunk_size: int
+        The chunk size of nodes created from data.
     test_distribution : dict
         Distribution of different types of questions to be generated from given
         set of documents. Defaults to {"easy":0.1, "reasoning":0.4, "conversation":0.5}
@@ -62,7 +108,7 @@ class TestsetGenerator:
         ctitic_llm: BaseLLM | BaseChatModel,
         embeddings_model: Embeddings,
         testset_distribution: t.Optional[t.Dict[str, float]] = None,
-        chat_qa: bool = True,
+        chat_qa: float = 0.3,
         chunk_size: int = 1024,
         seed: int = 42,
     ) -> None:
@@ -90,50 +136,61 @@ class TestsetGenerator:
         cls,
         openai_generator_llm: str = "gpt-3.5-turbo-16k",
         openai_filter_llm: str = "gpt-4",
+        chat_qa: float = 0.3,
+        chunk_size: int = 1024,
     ):
-        generator_llm = ChatOpenAI(model_name=openai_generator_llm)
-        ctitic_llm = ChatOpenAI(model_name=openai_filter_llm)
-        embeddings_model = OpenAIEmbedding()
+        generator_llm = ChatOpenAI(model=openai_generator_llm)
+        ctitic_llm = ChatOpenAI(model=openai_filter_llm)
+        embeddings_model = OpenAIEmbeddings()
         return cls(
             generator_llm=generator_llm,
             ctitic_llm=ctitic_llm,
             embeddings_model=embeddings_model,
+            chat_qa=chat_qa,
+            chunk_size=chunk_size,
         )
 
-    def _get_evolve_type(self):
+    def _get_evolve_type(self) -> str:
         prob = self.rng.uniform(0, 1)
-        for key in self.testset_distribution.keys():
-            if prob <= self.testset_distribution[key]:
-                return key
+        return next(
+            (
+                key
+                for key in self.testset_distribution.keys()
+                if prob <= self.testset_distribution[key]
+            ),
+            "simple",
+        )
 
-    def _filter_context(self, context: str):
+    def _filter_context(self, context: str) -> bool:
         human_prompt = SCORE_CONTEXT.format(context=context)
         prompt = ChatPromptTemplate.from_messages([human_prompt])
         results = generate(prompts=[prompt], llm=self.ctitic_llm)
         score = eval(results.generations[0][0].text.strip())
-        assert isinstance(score, float), "Score should be of type float"
+        if not isinstance(score, float | int):
+            score = 0.0
         return score >= self.threshold
 
-    def _seed_question(self, context: str):
+    def _seed_question(self, context: str) -> str:
         human_prompt = SEED_QUESTION.format(context=context)
         prompt = ChatPromptTemplate.from_messages([human_prompt])
         results = generate(prompts=[prompt], llm=self.generator_llm)
         return results.generations[0][0].text.strip()
 
-    def _filter_question(self, question: str):
-        prompt = ChatPromptTemplate.from_messages(
-            FILTER_QUESTION.format(question=question)
-        )
+    def _filter_question(self, question: str) -> bool:
+        human_prompt = FILTER_QUESTION.format(question=question)
+        prompt = ChatPromptTemplate.from_messages([human_prompt])
         results = generate(prompts=[prompt], llm=self.ctitic_llm)
-        return bool(results.generations[0][0].strip().endswith("Yes."))
+        return bool(results.generations[0][0].text.strip().endswith("Yes."))
 
-    def _reasoning_question(self, question: str, context: str):
+    def _reasoning_question(self, question: str, context: str) -> str:
         return self._qc_template(REASONING_QUESTION, question, context)
 
-    def _condition_question(self, question: str, context: str):
+    def _condition_question(self, question: str, context: str) -> str:
         return self._qc_template(CONDITIONAL_QUESTION, question, context)
 
-    def _multicontext_question(self, question: str, context1: str, context2: str):
+    def _multicontext_question(
+        self, question: str, context1: str, context2: str
+    ) -> str:
         human_prompt = MULTICONTEXT_QUESTION.format(
             question=question, context1=context1, context2=context2
         )
@@ -141,65 +198,67 @@ class TestsetGenerator:
         results = generate(prompts=[prompt], llm=self.generator_llm)
         return results.generations[0][0].text.strip()
 
-    def _compress_question(self, question: str):
+    def _compress_question(self, question: str) -> str:
         return self._question_transformation(COMPRESS_QUESTION, question=question)
 
-    def _conversational_question(self, question: str):
+    def _conversational_question(self, question: str) -> str:
         return self._question_transformation(CONVERSATION_QUESTION, question=question)
 
-    def _question_transformation(self, prompt, question: str):
+    def _question_transformation(self, prompt, question: str) -> str:
         human_prompt = prompt.format(question=question)
         prompt = ChatPromptTemplate.from_messages([human_prompt])
         results = generate(prompts=[prompt], llm=self.generator_llm)
         return results.generations[0][0].text.strip()
 
-    def _qc_template(self, prompt, question, context):
+    def _qc_template(self, prompt, question, context) -> str:
         human_prompt = prompt.format(question=question, context=context)
         prompt = ChatPromptTemplate.from_messages([human_prompt])
         results = generate(prompts=[prompt], llm=self.generator_llm)
         return results.generations[0][0].text.strip()
 
-    def _generate_answer(self, question: str, context: list[str]):
+    def _generate_answer(self, question: str, context: list[str]) -> t.List[str]:
         return [
             self._qc_template(ANSWER_FORMULATE, qstn, context[i])
             for i, qstn in enumerate(question.split("\n"))
         ]
 
-    def _generate_context(self, question: str, text_chunk: str):
+    def _generate_context(self, question: str, text_chunk: str) -> t.List[str]:
         return [
             self._qc_template(CONTEXT_FORMULATE, qstn, text_chunk)
             for qstn in question.split("\n")
         ]
 
-    def _remove_index(self, available_indices: list, node_idx: list):
+    def _remove_index(self, available_indices: list, node_idx: list) -> t.List:
         for idx in node_idx:
             available_indices.remove(idx)
         return available_indices
 
-    def _generate_doc_node_map(self, documenet_nodes: t.List[TextNode]):
+    def _generate_doc_node_map(
+        self, documenet_nodes: t.List[BaseNode]
+    ) -> t.Dict[str, list]:
         doc_nodeidx = defaultdict(list)
         for idx, node in enumerate(documenet_nodes):
             doc_nodeidx[node.id_].append(idx)
 
         return doc_nodeidx
 
-    def _get_neighbour_node(self, idx: int, node_indices: list):
+    def _get_neighbour_node(self, idx: int, node_indices: list) -> t.List[int]:
         return [idx - 1, idx] if idx == node_indices[-1] else [idx, idx + 1]
 
-    def _embed_nodes(self, nodes: t.List[TextNode]):
+    def _embed_nodes(self, nodes: t.List[BaseNode]) -> t.Dict[str, t.List[float]]:
         embeddings = {}
         for node in nodes:
-            embeddings[node.id_].update(
-                self.embedding_model.embed_query(node.get_context())
-            )
+            embeddings[node.id_] = list(self.embedding_model.embed_query(node.get_content()))
+        
 
         return embeddings
 
-    def generate(self, documents: t.List[Document], test_size: int):
+    def generate(self, documents: t.List[Document], test_size: int) -> TestDataset:
         node_parser = SimpleNodeParser.from_defaults(
             chunk_size=self.chunk_size, chunk_overlap=0, include_metadata=True
         )
-        document_nodes = node_parser.get_nodes_from_documents(documents=documents)
+        document_nodes: t.List[BaseNode] = node_parser.get_nodes_from_documents(documents=documents)
+        
 
         if test_size > len(document_nodes):
             raise ValueError(
@@ -210,21 +269,19 @@ class TestsetGenerator:
         available_indices = np.arange(0, len(document_nodes)).tolist()
         doc_nodeidx = self._generate_doc_node_map(document_nodes)
         count = 0
-        Testdata_tuple = namedtuple(
-            "Testdata_tuple", ["question", "context", "answer", "question_type"]
-        )
         samples = []
 
-        # TODO : Add progess bar
+        pbar = tqdm(total=test_size)
         while count < test_size and available_indices != []:
             size = self.rng.integers(1, 3)
+            evolve_type = self._get_evolve_type()
             node_idx = self.rng.choice(available_indices, size=1)[0]
             available_indices = self._remove_index(available_indices, [node_idx])
 
-            neighbor_nodes = doc_nodeidx[document_nodes[node_idx].id_]
+            neighbor_nodes = doc_nodeidx[document_nodes[node_idx].node_id]
             node_indices = (
                 self._get_neighbour_node(node_idx, neighbor_nodes)
-                if size > 1
+                if size > 1 and evolve_type != "multi_context"
                 else [node_idx]
             )
 
@@ -234,25 +291,28 @@ class TestsetGenerator:
             if not score:
                 continue
             seed_question = self._seed_question(text_chunk)
-            evolve_type = self._get_evolve_type()
 
-            if evolve_type == "multicontext":
+            if evolve_type == "multi_context":
                 node_embedding = self._embed_nodes([nodes[-1]])
                 neighbor_nodes = self._remove_index(neighbor_nodes, node_indices)
                 neighbor_emb = self._embed_nodes(
-                    [document_nodes[idx] for idx in neighbor_nodes]
+                    [document_nodes[idx][0] for idx in neighbor_nodes]
                 )
                 _, indices = get_top_k_embeddings(
-                    node_embedding, neighbor_emb, similarity_cutoff=self.threshold
+                    list(node_embedding.values())[0],
+                    list(neighbor_emb.values()),
+                    similarity_cutoff=self.threshold,
                 )
                 if indices:
                     best_neighbor = neighbor_nodes[indices[0]]
-                question = self._multicontext_question(
-                    question=seed_question,
-                    context1=text_chunk,
-                    context2=best_neighbor.get_content(),
-                )
-                text_chunk = "\n".join([text_chunk, best_neighbor.get_context()])
+                    question = self._multicontext_question(
+                        question=seed_question,
+                        context1=text_chunk,
+                        context2=best_neighbor.get_content(),
+                    )
+                    text_chunk = "\n".join([text_chunk, best_neighbor.get_context()])
+                else:
+                    continue
 
             else:
                 evolve_fun = question_deep_map.get(evolve_type)
@@ -264,7 +324,7 @@ class TestsetGenerator:
 
             if evolve_type != "simple":
                 prob = self.rng.uniform(0, 1)
-                if self.chat_qa and prob >= 0.5:
+                if self.chat_qa and prob <= self.chat_qa:
                     question = self._conversational_question(question=question)
                 else:
                     question = self._compress_question(question=question)
@@ -275,35 +335,6 @@ class TestsetGenerator:
                 Testdata_tuple(question.split("\n"), context, answer, evolve_type)
             )
             count += 1
+            pbar.update(count)
 
         return TestDataset(test_data=samples)
-
-
-@dataclass
-class TestDataset:
-    """
-    TestDataset class
-    """
-
-    test_data: t.Sequence[tuple[list[str], list[str], list[str], str]]
-
-    def to_pandas(self):
-        data_samples = []
-        for data in self.test_data:
-            is_conv = len(data.context) > 1
-            question_type = data.question_type
-            data = [
-                {
-                    "question": qstn,
-                    "context": ctx,
-                    "answer": ans,
-                    "question_type": question_type,
-                    "episode_done": True,
-                }
-                for qstn, ctx, ans in zip(data.question, data.context, data.answer)
-            ]
-            if is_conv:
-                data[-1] = data[0].update({"episode_done": False})
-            data_samples.extend(data)
-
-        return pd.DataFrame.from_records(data_samples)
