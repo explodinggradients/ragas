fd408a80d36a1bee5a9f5ab35dc6cb88f61ac979
jjmachan <jamesjithin97@gmail.com>
2023-09-26 15:31:58 +0530

    changed context_recall -> context_precision

--- src/ragas/metrics/__init__.py
+++ src/ragas/metrics/__init__.py
@@ -1,9 +1,15 @@
 from ragas.metrics.answer_relevance import AnswerRelevancy, answer_relevancy
+from ragas.metrics.context_precision import (
+    ContextPrecision,
+    ContextRelevancy,
+    context_precision,
+    context_relevancy,
+)
 from ragas.metrics.context_recall import ContextRecall, context_recall
-from ragas.metrics.context_relevance import ContextRelevancy, context_relevancy
 from ragas.metrics.critique import AspectCritique
-from ragas.metrics.faithfulnes import Faithfulness, faithfulness
+from ragas.metrics.faithfulness import Faithfulness, faithfulness
 
+# TODO: remove context_relevancy, ContextRelevancy after 0.1.0
 __all__ = [
     "Faithfulness",
     "faithfulness",
@@ -11,6 +17,8 @@ __all__ = [
     "answer_relevancy",
     "ContextRelevancy",
     "context_relevancy",
+    "ContextPrecision",
+    "context_precision",
     "AspectCritique",
     "ContextRecall",
     "context_recall",
