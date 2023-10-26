727f4b4974b84196b9ac8e37e868278adc2f16dd
Shahul ES <shahules786@gmail.com>
2023-10-26 12:22:15 +0530

    feat: context average precision (#204)
    - [x] Add AP from
    [here](https://amitness.com/2020/08/information-retrieval-evaluation/#2-average-precisionap)
    - [x] Rename context precision (depreciate)

--- src/ragas/metrics/context_precision.py
+++ src/ragas/metrics/context_precision.py
@@ -15,13 +15,22 @@ from sentence_transformers import CrossEncoder
 
 from ragas.metrics.base import EvaluationMode, MetricWithLLM
 
-CONTEXT_PRECISION = HumanMessagePromptTemplate.from_template(
+CONTEXT_RELEVANCE = HumanMessagePromptTemplate.from_template(
     """\
 Please extract relevant sentences from the provided context that is absolutely required answer the following question. If no relevant sentences are found, or if you believe the question cannot be answered from the given context, return the phrase "Insufficient Information".  While extracting candidate sentences you're not allowed to make any changes to sentences from given context.
 
 question:{question}
 context:\n{context}
 candidate sentences:\n"""  # noqa: E501
+)
+
+CONTEXT_PRECISION = HumanMessagePromptTemplate.from_template(
+    """\
+Given a question and a context, verify if the information in the given context is useful in answering the question. Return a Yes/No answer.
+question:{question}
+context:\n{context}
+answer:
+"""  # noqa: E501
 )
 
 
@@ -82,7 +91,7 @@ class SentenceAgreement:
 
 
 @dataclass
-class ContextPrecision(MetricWithLLM):
+class ContextRelevancy(MetricWithLLM):
     """
     Extracts sentences from the context that are relevant to the question with
     self-consistancy checks. The number of relevant sentences and is used as the score.
@@ -138,7 +147,7 @@ class ContextPrecision(MetricWithLLM):
             callback_group_name, callback_manager=callbacks
         ) as batch_group:
             for q, c in zip(questions, contexts):
-                human_prompt = CONTEXT_PRECISION.format(
+                human_prompt = CONTEXT_RELEVANCE.format(
                     question=q, context="\n".join(c)
                 )
                 prompts.append(ChatPromptTemplate.from_messages([human_prompt]))
@@ -177,9 +186,72 @@ class ContextPrecision(MetricWithLLM):
 
 
 @dataclass
-class ContextRelevancy(ContextPrecision):
-    name: str = "context_relevancy"
-    show_deprecation_warning: bool = True
+class ContextPrecision(MetricWithLLM):
+
+    """
+    Average Precision is a metric that evaluates whether all of the
+    relevant items selected by the model are ranked higher or not.
+
+    Attributes
+    ----------
+    name : str
+    batch_size : int
+        Batch size for openai completion.
+    """
+
+    name: str = "context_precision"
+    evaluation_mode: EvaluationMode = EvaluationMode.qc
+    batch_size: int = 15
+
+    def _score_batch(
+        self: t.Self,
+        dataset: Dataset,
+        callbacks: t.Optional[CallbackManager] = None,
+        callback_group_name: str = "batch",
+    ) -> list:
+        prompts = []
+        questions, contexts = dataset["question"], dataset["contexts"]
+        with trace_as_chain_group(
+            callback_group_name, callback_manager=callbacks
+        ) as batch_group:
+            for qstn, ctx in zip(questions, contexts):
+                human_prompts = [
+                    ChatPromptTemplate.from_messages(
+                        [CONTEXT_PRECISION.format(question=qstn, context=c)]
+                    )
+                    for c in ctx
+                ]
+
+                prompts.extend(human_prompts)
+
+            responses: list[list[str]] = []
+            results = self.llm.generate(
+                prompts,
+                n=1,
+                callbacks=batch_group,
+            )
+            responses = [[i.text for i in r] for r in results.generations]
+            context_lens = [len(ctx) for ctx in contexts]
+            context_lens.insert(0, 0)
+            context_lens = np.cumsum(context_lens)
+            grouped_responses = [
+                responses[start:end]
+                for start, end in zip(context_lens[:-1], context_lens[1:])
+            ]
+            scores = []
+
+            for response in grouped_responses:
+                response = [int("Yes" in resp) for resp in response]
+                denominator = sum(response) + 1e-10
+                numerator = sum(
+                    [
+                        (sum(response[: i + 1]) / (i + 1)) * response[i]
+                        for i in range(len(response))
+                    ]
+                )
+                scores.append(numerator / denominator)
+
+        return scores
 
 
 context_precision = ContextPrecision()
