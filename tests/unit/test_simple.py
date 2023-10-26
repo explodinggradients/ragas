83edb47ec561f35864846f1329c8c5d4d648f09d
jjmachan <jamesjithin97@gmail.com>
2023-10-26 12:20:57 +0530

    added test for type casting

--- tests/unit/test_simple.py
+++ tests/unit/test_simple.py
@@ -1,6 +1,13 @@
+import typing as t
+
+
 def test_import():
     import ragas
     from ragas.testset.testset_generator import TestsetGenerator
 
     assert TestsetGenerator is not None
     assert ragas is not None
+
+
+def test_type_casting():
+    t.cast(list[int], [1, 2, 3])
