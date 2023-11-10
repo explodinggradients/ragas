21ca07fbca9d2b824f18ac632cffd3c86720ffe0
Tino Max <tinothayil@gmail.com>
2023-11-09 22:20:13 -0600

    added variables names in tests

--- tests/unit/test_prompts.py
+++ tests/unit/test_prompts.py
@@ -14,15 +14,15 @@ context_precision = ContextPrecision(context_precision_prompt=test_prompt)
 context_relevancy = ContextRelevancy(context_relevance_prompt=test_prompt)
 context_recall = ContextRecall(context_recall_prompt=test_prompt)
 critique = AspectCritique(name="test_critique", definition="This is a test critique", critique_prompt=test_prompt)
-faithfulness = Faithfulness(long_answer_prompt=test_prompt, nli_stmt_prompt=test_prompt)
+faithfulness = Faithfulness(long_answer_prompt=test_prompt, nli_statements_prompt=test_prompt)
 
 
 def test_prompts():
 
-    assert answer_relevancy.question_gen_prompt==test_prompt, "Answer relevancy custom prompt error"
+    assert answer_relevancy.question_generation_prompt==test_prompt, "Answer relevancy custom prompt error"
     assert context_precision.context_precision_prompt==test_prompt, "Context precision custom prompt error"
     assert context_relevancy.context_relevance_prompt==test_prompt, "Context relevancy custom prompt error"
     assert context_recall.context_recall_prompt==test_prompt, "Context recall custom prompt error"
     assert critique.critique_prompt==test_prompt, "Critique custom prompt error"
     assert faithfulness.long_answer_prompt==test_prompt, "Faithfulness custom prompt error"
-    assert faithfulness.nli_stmt_prompt==test_prompt, "Faithfulness custom prompt error"
+    assert faithfulness.nli_statements_prompt==test_prompt, "Faithfulness custom prompt error"
