8279f8b870b3fee2367cefb368d6fc38042dad81
Shahules786 <Shahules786@gmail.com>
2023-12-18 20:57:33 +0530

    added GT context precision

--- src/ragas/metrics/_context_precision.py
+++ src/ragas/metrics/_context_precision.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import typing as t
 from dataclasses import dataclass
 
@@ -15,25 +16,42 @@ if t.TYPE_CHECKING:
     from langchain.callbacks.base import Callbacks
 
 CONTEXT_PRECISION = HumanMessagePromptTemplate.from_template(
-    """\
-Verify if the information in the given context is useful in answering the question. Use only "Yes" (1) or "No" (0) as a binary verdict.
+    """Given question, answer and context verify if the context was useful in arriving at the given answer. Give verdict as "1" if useful and "0" if not. 
 
-question: What are the health benefits of green tea?
-context: 
-This article explores the rich history of tea cultivation in China, tracing its roots back to the ancient dynasties. It discusses how different regions have developed their unique tea varieties and brewing techniques. The article also delves into the cultural significance of tea in Chinese society and how it has become a symbol of hospitality and relaxation.
+question: What can you tell me about albert Albert Einstein?
+context: Albert Einstein (14 March 1879 – 18 April 1955) was a German-born theoretical physicist,widely held to be one of the greatest and most influential scientists of all time. Best known for developing the theory of relativity, he also made important contributions to quantum mechanics, and was thus a central figure in the revolutionary reshaping of the scientific understanding of nature that modern physics accomplished in the first decades of the twentieth century. His mass–energy equivalence formula E = mc2, which arises from relativity theory, has been called "the world's most famous equation". He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect", a pivotal step in the development of quantum theory. His work is also known for its influence on the philosophy of science. In a 1999 poll of 130 leading physicists worldwide by the British journal Physics World, Einstein was ranked the greatest physicist of all time. His intellectual achievements and originality have made Einstein synonymous with genius.
+answer: Albert Einstein born in 14 March 1879 was  German-born theoretical physicist, widely held to be one of the greatest and most influential scientists of all time. He received the 1921 Nobel Prize in Physics "for his services to theoretical physics. He published 4 papers in 1905.  Einstein moved to Switzerland in 1895 
 verification:
-{{"reason":"The context, while informative about the history and cultural significance of tea in China, does not provide specific information about the health benefits of green tea. Thus, it is not useful for answering the question about health benefits.", "verdict":"0"}}
+{{  
+    "reason": "The provided context was indeed useful in arriving at the given answer. The context includes key information about Albert Einstein's life and contributions, which are reflected in the answer.",
+    "Verdict": "1"
+}}
 
-question: How does photosynthesis work in plants?
-context:
-Photosynthesis in plants is a complex process involving multiple steps. This paper details how chlorophyll within the chloroplasts absorbs sunlight, which then drives the chemical reaction converting carbon dioxide and water into glucose and oxygen. It explains the role of light and dark reactions and how ATP and NADPH are produced during these processes.
+question: who won 2020 icc world cup?
+context: Who won the 2022 ICC Men's T20 World Cup?
+The 2022 ICC Men's T20 World Cup, held from October 16 to November 13, 2022, in Australia, was the eighth edition of the tournament. Originally scheduled for 2020, it was postponed due to the COVID-19 pandemic. England emerged victorious, defeating Pakistan by five wickets in the final to clinch their second ICC Men's T20 World Cup title.
+answer: England 
 verification:
-{{"reason":"This context is extremely relevant and useful for answering the question. It directly addresses the mechanisms of photosynthesis, explaining the key components and processes involved.", "verdict":"1"}}
+{{
+        "reason": "the context was useful in clarifying the situation regarding the 2020 ICC World Cup and indicating that England was the winner of the tournament that was intended to be held in 2020 but actually took place in 2022.",
+         "verdict": "1"
+}}
+
+
+question: What is the tallest mountain in the world?
+context: The Andes is the longest continental mountain range in the world, located in South America. It stretches across seven countries and features many of the highest peaks in the Western Hemisphere. The range is known for its diverse ecosystems, including the high-altitude Andean Plateau and the Amazon rainforest.
+answer: Mount Everest.
+verification:
+{{
+    "reason":"the provided context discusses the Andes mountain range, which, while impressive, does not include Mount Everest or directly relate to the question about the world's tallest mountain.",
+    "verdict":"0"
+}}
+
 
 question:{question}
-context:
-{context}
-verification:"""  # noqa: E501
+context:{context}
+answer:{answer}
+verification:"""
 )
 
 
@@ -54,6 +72,16 @@ class ContextPrecision(MetricWithLLM):
     evaluation_mode: EvaluationMode = EvaluationMode.qc  # type: ignore
     batch_size: int = 15
 
+    def get_dataset_attributes(self, dataset: Dataset):
+        answer = "ground_truths"
+        if answer not in dataset.features.keys():
+            logging.warning(
+                "Using context precision without ground truth will be soon depreciated. Use context_utilization instead"
+            )
+            answer = "answer"
+
+        return dataset["question"], dataset["contexts"], dataset[answer]
+
     def _score_batch(
         self: t.Self,
         dataset: Dataset,
@@ -61,16 +89,20 @@ class ContextPrecision(MetricWithLLM):
         callback_group_name: str = "batch",
     ) -> list:
         prompts = []
-        questions, contexts = dataset["question"], dataset["contexts"]
+        questions, contexts, answers = self.get_dataset_attributes(dataset)
 
         cb = CallbackManager.configure(inheritable_callbacks=callbacks)
         with trace_as_chain_group(
             callback_group_name, callback_manager=cb
         ) as batch_group:
-            for qstn, ctx in zip(questions, contexts):
+            for qstn, ctx, answer in zip(questions, contexts, answers):
                 human_prompts = [
                     ChatPromptTemplate.from_messages(
-                        [CONTEXT_PRECISION.format(question=qstn, context=c)]
+                        [
+                            CONTEXT_PRECISION.format(
+                                question=qstn, context=c, answer=answer
+                            )
+                        ]
                     )
                     for c in ctx
                 ]
@@ -115,4 +147,13 @@ class ContextPrecision(MetricWithLLM):
         return scores
 
 
+class ContextUtilization(ContextPrecision):
+    name = "ContextUtilization"
+    evaluation_mode = EvaluationMode.qac
+
+    def get_dataset_attributes(self, dataset: Dataset):
+        return dataset["question"], dataset["contexts"], dataset["answer"]
+
+
 context_precision = ContextPrecision()
+context_utilization = ContextUtilization()
