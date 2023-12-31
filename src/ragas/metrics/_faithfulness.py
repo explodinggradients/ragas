84cb885d4bb26073bc1bd43f563238699f5c6cd7
Tino Max Thayil <tinothayil@gmail.com>
2023-12-30 22:44:53 -0600

    added prompt objects to metrics (#401)
    * Added prompt objects to metrics
    
    ---------
    
    Co-authored-by: Shahules786 <Shahules786@gmail.com>

--- src/ragas/metrics/_faithfulness.py
+++ src/ragas/metrics/_faithfulness.py
@@ -5,118 +5,113 @@ from dataclasses import dataclass
 
 import numpy as np
 from langchain.callbacks.manager import CallbackManager, trace_as_chain_group
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 
-from ragas.metrics.base import EvaluationMode, MetricWithLLM
 from ragas.utils import json_loader
+from ragas.llms.prompt import Prompt
+from ragas.metrics.base import EvaluationMode, MetricWithLLM
 
 if t.TYPE_CHECKING:
     from datasets import Dataset
     from langchain.callbacks.base import Callbacks
 
 
-LONG_FORM_ANSWER_PROMPT = HumanMessagePromptTemplate.from_template(
-    """\
-Create one or more statements from each sentence in the given answer.
-
-question: Who was  Albert Einstein and what is he best known for?
-answer: He was a German-born theoretical physicist, widely acknowledged to be one of the greatest and most influential physicists of all time. He was best known for developing the theory of relativity, he also made important contributions to the development of the theory of quantum mechanics.
-statements in json:
-{{
-    "statements": [
-        "Albert Einstein was born in Germany.",
-        "Albert Einstein was best known for his theory of relativity."
-    ]
-}}
-
-question: Cadmium Chloride is slightly soluble in this chemical, it is also called what?
-answer: alcohol
-statements in json:
-{{
-    "statements": [
-        "Cadmium Chloride is slightly soluble in alcohol."
-    ]
-}}
-
-question: Were Hitler and Benito Mussolini of the same nationality?
-answer: Sorry, I can't provide answer to that question.
-statements in json:
-{{
-    "statements": []
-}}
-
-question:{question}
-answer: {answer}
-statements in json:"""  # noqa: E501
-)
-
-
-NLI_STATEMENTS_MESSAGE = HumanMessagePromptTemplate.from_template(
-    """
- Natural language inference. Use only "Yes" (1), "No" (0) and "Null" (-1) as verdict.
-
-Context:
-John is a student at XYZ University. He is pursuing a degree in Computer Science. He is enrolled in several courses this semester, including Data Structures, Algorithms, and Database Management. John is a diligent student and spends a significant amount of time studying and completing assignments. He often stays late in the library to work on his projects.
-statement_1: John is majoring in Biology.
-statement_2: John is taking a course on Artificial Intelligence. 
-statement_3: John is a dedicated student. 
-statement_4: John has a part-time job.
-Answer:
-[
-    {{
-        "statement_1": "John is majoring in Biology.",
-        "reason": "John's major is explicitly mentioned as Computer Science. There is no information suggesting he is majoring in Biology.",
-        "verdict": "0"
-    }},
-    {{
-        "statement_2": "John is taking a course on Artificial Intelligence.",
-        "reason": "The context mentions the courses John is currently enrolled in, and Artificial Intelligence is not mentioned. Therefore, it cannot be deduced that John is taking a course on AI.",
-        "verdict": "0"
-    }},
-    {{
-        "statement_3": "John is a dedicated student.",
-        "reason": "The context states that he spends a significant amount of time studying and completing assignments. Additionally, it mentions that he often stays late in the library to work on his projects, which implies dedication.",
-        "verdict": "1"
-    }},
-    {{
-        "statement_4": "John has a part-time job.",
-        "reason": "There is no information given in the context about John having a part-time job.",
-        "verdict": "0"
-    }}
-]
-
-Context:
-Photosynthesis is a process used by plants, algae, and certain bacteria to convert light energy into chemical energy.
-statement_1: Albert Einstein was a genius.
-Answer:
-[
-     {{
-        "statement_1": "Albert Einstein was a genius.",
-        "reason": "The context and statement are unrelated"
-        "verdict": "0"
-    }}
-]
-
-Context:
-Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.
-statement_1: Nil
-Answer:
-[
-     {{
-        "statement_1": "Nil",
-        "reason": "The statement is invalid",
-        "verdict": "-1"
-    }}
-]
+LONG_FORM_ANSWER_PROMPT = Prompt(
+    instruction="Create one or more statements from each sentence in the given answer.",
+    examples=[
+        {
+            "question": "Who was  Albert Einstein and what is he best known for?",
+            "answer": "He was a German-born theoretical physicist, widely acknowledged to be one of the greatest and most influential physicists of all time. He was best known for developing the theory of relativity, he also made important contributions to the development of the theory of quantum mechanics.",
+            "statements": """{
+                "statements": [
+                    "Albert Einstein, a German-born theoretical physicist, is renowned for being one of the most influential physicists in history.",
+                    "Albert Einstein was best known for his theory of relativity.",
+                    "Einstein's contributions significantly advanced the field of quantum mechanics",
+                    "Recognized globally, Einstein's work has profoundly impacted the scientific community",
+                    "Einstein's groundbreaking theories continue to shape our understanding of physics today."
+                ]
+            }""",
+        },
+        {
+            "question": "Cadmium Chloride is slightly soluble in this chemical, it is also called what?",
+            "answer": "alcohol",
+            "statements": """{
+                "statements": [
+                    "Cadmium Chloride is slightly soluble in alcohol."
+                ]
+            }""",
+        },
+        {
+            "question": "Were Hitler and Benito Mussolini of the same nationality?",
+            "answer": "Sorry, I can't provide answer to that question.",
+            "statements": """{
+                "statements": []
+            }""",
+        },
+    ],
+    input_keys=["question", "answer"],
+    output_key="statements",
+    output_type="JSON",
+)  # noqa: E501
 
 
-context:
-{context}
-statements:
-{statements}
-Answer:
-"""  # noqa: E501
-)
+NLI_STATEMENTS_MESSAGE = Prompt(
+    instruction="Natural language inference. Use only 'Yes' (1), 'No' (0) and 'Null' (-1) as verdict.",
+    examples=[
+        {
+            "context": """John is a student at XYZ University. He is pursuing a degree in Computer Science. He is enrolled in several courses this semester, including Data Structures, Algorithms, and Database Management. John is a diligent student and spends a significant amount of time studying and completing assignments. He often stays late in the library to work on his projects.""",
+            "statements": """
+            statement_1: John is majoring in Biology.
+            statement_2: John is taking a course on Artificial Intelligence. 
+            statement_3: John is a dedicated student. 
+            statement_4: John has a part-time job.
+            """,
+            "answer": """[
+            {
+                "statement_1": "John is majoring in Biology.",
+                "reason": "John's major is explicitly mentioned as Computer Science. There is no information suggesting he is majoring in Biology.",
+                "verdict": "0"
+            },
+            {
+                "statement_2": "John is taking a course on Artificial Intelligence.",
+                "reason": "The context mentions the courses John is currently enrolled in, and Artificial Intelligence is not mentioned. Therefore, it cannot be deduced that John is taking a course on AI.",
+                "verdict": "0"
+            },
+            {
+                "statement_3": "John is a dedicated student.",
+                "reason": "The context states that he spends a significant amount of time studying and completing assignments. Additionally, it mentions that he often stays late in the library to work on his projects, which implies dedication.",
+                "verdict": "1"
+            },
+            {
+                "statement_4": "John has a part-time job.",
+                "reason": "There is no information given in the context about John having a part-time job.",
+                "verdict": "0"
+            }
+        ]
+            """,
+        },
+        {
+            "context": """Photosynthesis is a process used by plants, algae, and certain bacteria to convert light energy into chemical energy.""",
+            "statements": """statement_1: Albert Einstein was a genius.""",
+            "answer": """{
+                "statement_1": "Albert Einstein was a genius.",
+                "reason": "The context and statement are unrelated",
+                "verdict": "0"
+            }""",
+        },
+        {
+            "context": """Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.""",
+            "statements": """statement_1: Nil""",
+            "answer": """{
+                "statement_1": "Nil",
+                "reason": "The statement is invalid",
+                "verdict": "-1"
+            }""",
+        },
+    ],
+    input_keys=["context", "statements"],
+    output_key="answer",
+    output_type="JSON",
+)  # noqa: E501
 
 
 @dataclass
@@ -148,7 +143,7 @@ class Faithfulness(MetricWithLLM):
         ) as batch_group:
             for q, a in zip(question, answer):
                 human_prompt = LONG_FORM_ANSWER_PROMPT.format(question=q, answer=a)
-                prompts.append(ChatPromptTemplate.from_messages([human_prompt]))
+                prompts.append(human_prompt)
 
             result = self.llm.generate(prompts, callbacks=batch_group)
 
@@ -165,15 +160,15 @@ class Faithfulness(MetricWithLLM):
                 human_prompt = NLI_STATEMENTS_MESSAGE.format(
                     context=contexts_str, statements=statements_str
                 )
-                prompts.append(ChatPromptTemplate.from_messages([human_prompt]))
+                prompts.append(human_prompt)
 
             result = self.llm.generate(prompts, callbacks=batch_group)
             outputs = result.generations
-            verdict_score_map = {"1": 1, "0": 0, "-1": np.nan}
+            verdict_score_map = {"1": 1, "0": 0, "null": np.nan}
             scores = []
             for output in outputs:
                 output = json_loader.safe_load(output[0].text, self.llm)
-                output = output if isinstance(output, list) else []
+                output = output if isinstance(output, list) else [output]
                 faithful_statements = sum(
                     verdict_score_map.get(dict.get("verdict", "").lower(), np.nan)
                     for dict in output
