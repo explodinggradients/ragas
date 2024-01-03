36916535f27476bb612026be625d35be75d54754
jjmachan <jamesjithin97@gmail.com>
2024-01-03 12:24:33 +0530

    added benchmark

--- tests/benchmarks/benchmark_eval.py
+++ tests/benchmarks/benchmark_eval.py
@@ -3,32 +3,52 @@ import time
 from datasets import DatasetDict, load_dataset
 
 from ragas import evaluate
-from ragas.metrics import faithfulness
+from ragas.metrics import (
+    faithfulness,
+    context_recall,
+    answer_correctness,
+    context_relevancy,
+    context_precision,
+    context_utilization,
+    answer_similarity,
+)
+from ragas.metrics.critique import harmfulness
 
 # data
 ds = load_dataset("explodinggradients/fiqa", "ragas_eval")
 assert isinstance(ds, DatasetDict)
 fiqa = ds["baseline"]
 
+# metrics
+metrics = [
+    faithfulness,
+    context_recall,
+    answer_correctness,
+    harmfulness,
+    context_relevancy,
+    context_precision,
+    context_utilization,
+    answer_similarity,
+]
+
 if __name__ == "__main__":
     # asyncio
     start = time.time()
-    _ = evaluate(
-        fiqa,
-        metrics=[
-            faithfulness,
-        ],
-        is_async=True,
-    )
+    print("ignored")
+    # _ = evaluate(
+    #     fiqa,
+    #     metrics=[
+    #         faithfulness,
+    #     ],
+    #     is_async=True,
+    # )
     print(f"Time taken [Asyncio]: {time.time() - start:.2f}s")
 
     # Threads
     start = time.time()
     _ = evaluate(
         fiqa,
-        metrics=[
-            faithfulness,
-        ],
+        metrics=metrics,
         is_async=False,
     )
     print(f"Time taken [Threads]: {time.time() - start:.2f}s")
