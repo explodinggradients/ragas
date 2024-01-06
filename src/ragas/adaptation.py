3f44be326f91452da5059ad8632e254b45ce4b58
Shahules786 <Shahules786@gmail.com>
2024-01-07 00:41:04 +0530

    git global adaptation

---
+++ src/ragas/adaptation.py
@@ -0,0 +1,32 @@
+import typing as t
+
+from langchain_core.language_models import BaseLanguageModel
+
+from ragas.llms import BaseRagasLLM, LangchainLLMWrapper, llm_factory
+from ragas.metrics.base import MetricWithLLM
+
+
+def adapt(
+    metrics: t.List[MetricWithLLM],
+    language: str,
+    llm: t.Optional[BaseRagasLLM] = None,
+    cache_dir: str | None = None,
+) -> None:
+    """
+    Adapt the metric to a different language.
+    """
+
+    llm_wraper = None
+
+    if llm is None:
+        llm_wraper = llm_factory()
+    elif isinstance(llm, BaseLanguageModel):
+        llm_wraper = LangchainLLMWrapper(llm)
+
+    for metric in metrics:
+        if metric.llm is None or llm is not None:
+            metric.llm = llm_wraper
+
+        if hasattr(metric, "adapt"):
+            metric.adapt(language, cache_dir=cache_dir)
+            metric.save(cache_dir=cache_dir)
