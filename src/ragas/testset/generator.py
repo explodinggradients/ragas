40ac54f56d271c0c8c12744f33588c617312a530
Shahules786 <Shahules786@gmail.com>
2024-01-27 19:17:44 -0800

    to_pandas

--- src/ragas/testset/generator.py
+++ src/ragas/testset/generator.py
@@ -5,6 +5,7 @@ import typing as t
 from dataclasses import dataclass
 
 import pandas as pd
+from datasets import Dataset
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_openai.embeddings import OpenAIEmbeddings
 
@@ -41,14 +42,19 @@ class TestDataset:
 
     test_data: t.List[DataRow]
 
-    def to_pandas(self) -> pd.DataFrame:
+    def _to_records(self) -> t.List[t.Dict]:
         data_samples = []
         for data in self.test_data:
             data_dict = dict(data)
             data_dict["episode_done"] = True
             data_samples.append(data_dict)
+        return data_samples
 
-        return pd.DataFrame.from_records(data_samples)
+    def to_pandas(self) -> pd.DataFrame:
+        return pd.DataFrame.from_records(self._to_records())
+
+    def to_dataset(self) -> Dataset:
+        return Dataset.from_list(self._to_records())
 
 
 @dataclass
@@ -77,7 +83,7 @@ class TestsetGenerator:
             docstore = InMemoryDocumentStore(
                 splitter=splitter,
                 embeddings=embeddings_model,
-                generator_llm=generator_llm_model,
+                llm=generator_llm_model,
             )
             return cls(
                 generator_llm=generator_llm_model,
