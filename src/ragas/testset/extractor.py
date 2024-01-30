797a2798806f4e329921cea82a9ccdbc17c9eff1
Shahules786 <Shahules786@gmail.com>
2024-01-29 16:37:59 -0800

    add exctractor

--- src/ragas/testset/extractor.py
+++ src/ragas/testset/extractor.py
@@ -1,28 +1,31 @@
 from __future__ import annotations
 
 import typing as t
-from abc import abstractmethod, ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
-from ragas.llms.base import BaseRagasLLM
+from ragas.llms.json_load import json_loader
+from ragas.testset.prompts import keyphrase_extraction_prompt
 
 if t.TYPE_CHECKING:
     from ragas.llms.base import BaseRagasLLM
     from ragas.testset.docstore import Node
 
+
 @dataclass
 class Extractor(ABC):
     llm: BaseRagasLLM
-    
+
     @abstractmethod
     def extract(self, node: Node) -> t.Any:
         ...
-        
-  
-      
+
+
 class keyphraseExtractor(Extractor):
-    
-    
-    def extract(self, node: Node) -> t.Any:
-        self.llm.
-    
+    async def extract(self, node: Node) -> t.List[str]:
+        prompt = keyphrase_extraction_prompt.format(text=node.page_content)
+        results = await self.llm.agenerate_text(prompt=prompt)
+        keyphrases = json_loader._safe_load(
+            results.generations[0][0].text.strip(), llm=self.llm
+        )
+        return keyphrases.get("keyphrases", [])
