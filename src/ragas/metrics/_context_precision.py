8b498cd02ffc724fbfcc7cebfac4386ffdcc810f
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2024-03-21 18:48:06 +0100

    Implement output parsing in context precision

--- src/ragas/metrics/_context_precision.py
+++ src/ragas/metrics/_context_precision.py
@@ -6,46 +6,61 @@ from dataclasses import dataclass, field
 
 import numpy as np
 from datasets import Dataset
+from langchain.pydantic_v1 import BaseModel, Field, ValidationError
 
 from ragas.llms.json_load import json_loader
 from ragas.llms.prompt import Prompt, PromptValue
 from ragas.metrics.base import EvaluationMode, MetricWithLLM
+from ragas.llms.output_parser import get_json_format_instructions
 
 if t.TYPE_CHECKING:
     from langchain_core.callbacks import Callbacks
 
 logger = logging.getLogger(__name__)
 
+
+class ContextPrecisionVerification(BaseModel):
+    """Answer for the verification task wether the context was useful."""
+    reason: str = Field(..., description="Reason for verification")
+    verdict: bool = Field(..., description="Verdict of verification")
+
+class ContextPrecisionVerifications(BaseModel):
+    __root__: t.List[ContextPrecisionVerification]
+
+_verification_output_instructions = get_json_format_instructions(ContextPrecisionVerification)
+
+
 CONTEXT_PRECISION = Prompt(
     name="context_precision",
-    instruction="""Given question, answer and context verify if the context was useful in arriving at the given answer. Give verdict as "1" if useful and "0" if not with json output. """,
+    instruction="""Given question, answer and context verify if the context was useful in arriving at the given answer. Give verdict as "true" if useful and "false" if not with json output. Give a reason for your verdict.""",
+    output_format_instruction=_verification_output_instructions,
     examples=[
         {
             "question": """What can you tell me about albert Albert Einstein?""",
             "context": """Albert Einstein (14 March 1879 – 18 April 1955) was a German-born theoretical physicist, widely held to be one of the greatest and most influential scientists of all time. Best known for developing the theory of relativity, he also made important contributions to quantum mechanics, and was thus a central figure in the revolutionary reshaping of the scientific understanding of nature that modern physics accomplished in the first decades of the twentieth century. His mass–energy equivalence formula E = mc2, which arises from relativity theory, has been called "the world's most famous equation". He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect", a pivotal step in the development of quantum theory. His work is also known for its influence on the philosophy of science. In a 1999 poll of 130 leading physicists worldwide by the British journal Physics World, Einstein was ranked the greatest physicist of all time. His intellectual achievements and originality have made Einstein synonymous with genius.""",
             "answer": """Albert Einstein born in 14 March 1879 was German-born theoretical physicist, widely held to be one of the greatest and most influential scientists of all time. He received the 1921 Nobel Prize in Physics for his services to theoretical physics. He published 4 papers in 1905. Einstein moved to Switzerland in 1895""",
-            "verification": {
-                "reason": "The provided context was indeed useful in arriving at the given answer. The context includes key information about Albert Einstein's life and contributions, which are reflected in the answer.",
-                "verdict": "1",
-            },
+            "verification": ContextPrecisionVerification(
+                reason="The provided context was indeed useful in arriving at the given answer. The context includes key information about Albert Einstein's life and contributions, which are reflected in the answer.",
+                verdict=True,
+            ).dict(),
         },
         {
             "question": """who won 2020 icc world cup?""",
             "context": """The 2022 ICC Men's T20 World Cup, held from October 16 to November 13, 2022, in Australia, was the eighth edition of the tournament. Originally scheduled for 2020, it was postponed due to the COVID-19 pandemic. England emerged victorious, defeating Pakistan by five wickets in the final to clinch their second ICC Men's T20 World Cup title.""",
             "answer": """England""",
-            "verification": {
-                "reason": "the context was useful in clarifying the situation regarding the 2020 ICC World Cup and indicating that England was the winner of the tournament that was intended to be held in 2020 but actually took place in 2022.",
-                "verdict": "1",
-            },
+            "verification": ContextPrecisionVerification(
+                reason="the context was useful in clarifying the situation regarding the 2020 ICC World Cup and indicating that England was the winner of the tournament that was intended to be held in 2020 but actually took place in 2022.",
+                verdict=True,
+            ).dict(),
         },
         {
             "question": """What is the tallest mountain in the world?""",
             "context": """The Andes is the longest continental mountain range in the world, located in South America. It stretches across seven countries and features many of the highest peaks in the Western Hemisphere. The range is known for its diverse ecosystems, including the high-altitude Andean Plateau and the Amazon rainforest.""",
             "answer": """Mount Everest.""",
-            "verification": {
-                "reason": "the provided context discusses the Andes mountain range, which, while impressive, does not include Mount Everest or directly relate to the question about the world's tallest mountain.",
-                "verdict": "0",
-            },
+            "verification": ContextPrecisionVerification(
+                reason="the provided context discusses the Andes mountain range, which, while impressive, does not include Mount Everest or directly relate to the question about the world's tallest mountain.",
+                verdict=False,
+            ).dict(),
         },
     ],
     input_keys=["question", "context", "answer"],
@@ -92,14 +107,22 @@ class ContextPrecision(MetricWithLLM):
 
     def _calculate_average_precision(self, json_responses: t.List[t.Dict]) -> float:
         score = np.nan
-        json_responses = [
-            item if isinstance(item, dict) else {} for item in json_responses
-        ]
+
+        try:
+            responses = ContextPrecisionVerifications.parse_obj(json_responses).__root__
+        except ValidationError as err:
+            logger.warning(f"Could not parse LLM response: {json_responses}")
+            logger.warning(f"Error: {err}")
+            return np.nan
+
+        # TODO: real error handling and retry?
+        # https://python.langchain.com/docs/modules/model_io/output_parsers/types/retry
+
+
+
         verdict_list = [
-            int("1" == resp.get("verdict", "").strip())
-            if resp.get("verdict")
-            else np.nan
-            for resp in json_responses
+            1 if resp.verdict else 0
+            for resp in responses
         ]
         denominator = sum(verdict_list) + 1e-10
         numerator = sum(
