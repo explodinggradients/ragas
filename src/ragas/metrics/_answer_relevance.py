e74839f786a85a209834b23565b82b85536b58d2
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2024-03-26 16:12:29 +0100

    Implement pydantic parser in answer relevancy

--- src/ragas/metrics/_answer_relevance.py
+++ src/ragas/metrics/_answer_relevance.py
@@ -5,10 +5,14 @@ import typing as t
 from dataclasses import dataclass, field
 
 import numpy as np
+from langchain_core.pydantic_v1 import BaseModel, ValidationError
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.exceptions import OutputParserException
 
 from ragas.llms.json_load import json_loader
 from ragas.llms.prompt import Prompt
 from ragas.metrics.base import EvaluationMode, MetricWithEmbeddings, MetricWithLLM
+from ragas.llms.output_parser import get_json_format_instructions
 
 logger = logging.getLogger(__name__)
 
@@ -17,41 +21,52 @@ if t.TYPE_CHECKING:
 
     from ragas.llms.prompt import PromptValue
 
+
+class AnswerRelevanceClassification(BaseModel):
+    question: str
+    noncommittal: bool
+
+
+_output_instructions = get_json_format_instructions(pydantic_object=AnswerRelevanceClassification)
+_output_parser = PydanticOutputParser(pydantic_object=AnswerRelevanceClassification)
+
+
 QUESTION_GEN = Prompt(
     name="question_generation",
-    instruction="""Generate a question for the given answer and Identify if answer is noncommittal. Give noncommittal as 1 if the answer is noncommittal and 0 if the answer is committal. A noncommittal answer is one that is evasive, vague, or ambiguous. For example, "I don't know" or "I'm not sure" are noncommittal answers.""",
+    instruction="""Generate a question for the given answer and information context, and identify if the answer is noncommittal. A noncommittal answer is one that is evasive, vague, or ambiguous. For example, "I don't know" or "I'm not sure" are noncommittal answers. Give noncommittal as "true" if the answer is noncommittal and "false" if the answer is committal.""",
+    output_format_instruction=_output_instructions,
     examples=[
         {
             "answer": """Albert Einstein was born in Germany.""",
             "context": """Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time""",
-            "output": {
+            "output": AnswerRelevanceClassification.parse_obj({
                 "question": "Where was Albert Einstein born?",
-                "noncommittal": 0,
-            },
+                "noncommittal": False,
+            }).dict(),
         },
         {
             "answer": """It can change its skin color based on the temperature of its environment.""",
             "context": """A recent scientific study has discovered a new species of frog in the Amazon rainforest that has the unique ability to change its skin color based on the temperature of its environment.""",
-            "output": {
+            "output": AnswerRelevanceClassification.parse_obj({
                 "question": "What unique ability does the newly discovered species of frog have?",
-                "noncommittal": 0,
-            },
+                "noncommittal": False,
+            }).dict(),
         },
         {
             "answer": """Everest""",
             "context": """The tallest mountain on Earth, measured from sea level, is a renowned peak located in the Himalayas.""",
-            "output": {
+            "output": AnswerRelevanceClassification.parse_obj({
                 "question": "What is the tallest mountain on Earth?",
-                "noncommittal": 0,
-            },
+                "noncommittal": False,
+            }).dict(),
         },
         {
             "answer": """I don't know about the  groundbreaking feature of the smartphone invented in 2023 as am unaware of information beyond 2022. """,
             "context": """In 2023, a groundbreaking invention was announced: a smartphone with a battery life of one month, revolutionizing the way people use mobile technology.""",
-            "output": {
+            "output": AnswerRelevanceClassification.parse_obj({
                 "question": "What was the groundbreaking feature of the smartphone invented in 2023?",
-                "noncommittal": 1,
-            },
+                "noncommittal": True,
+            }).dict(),
         },
     ],
     input_keys=["answer", "context"],
@@ -83,6 +98,7 @@ class AnswerRelevancy(MetricWithLLM, MetricWithEmbeddings):
     evaluation_mode: EvaluationMode = EvaluationMode.qac  # type: ignore
     question_generation: Prompt = field(default_factory=lambda: QUESTION_GEN)
     strictness: int = 3
+    use_langchain_parser: bool = False
 
     def calculate_similarity(
         self: t.Self, question: str, generated_questions: list[str]
@@ -102,16 +118,15 @@ class AnswerRelevancy(MetricWithLLM, MetricWithEmbeddings):
             / norm
         )
 
-    def _calculate_score(self, response: t.Sequence[t.Any], row: t.Dict) -> float:
+    def _calculate_score(self, answers: t.Sequence[AnswerRelevanceClassification], row: t.Dict) -> float:
         question = row["question"]
         gen_questions = [
-            item.get("question", "") for item in response if isinstance(item, dict)
+            answer.question for answer in answers
         ]
         committal = np.any(
             [
-                bool(item.get("noncommittal", 0))
-                for item in response
-                if isinstance(item, dict)
+                answer.noncommittal
+                for answer in answers
             ]
         )
         if all(q == "" for q in gen_questions):
@@ -139,12 +154,31 @@ class AnswerRelevancy(MetricWithLLM, MetricWithEmbeddings):
             callbacks=callbacks,
             is_async=is_async,
         )
-        response = [
-            await json_loader.safe_load(r.text, self.llm, is_async=is_async)
-            for r in result.generations[0]
-        ]
 
-        return self._calculate_score(response, row)
+        try:
+
+            if self.use_langchain_parser:
+                answers = [
+                    _output_parser.parse(result.text)
+                    for result in result.generations[0]
+                ]
+                # TODO: real error handling and retry?
+                # https://python.langchain.com/docs/modules/model_io/output_parsers/types/retry
+            else:
+                responses = [
+                    await json_loader.safe_load(r.text, self.llm, is_async=is_async)
+                    for r in result.generations[0]
+                ]
+                answers = [
+                    AnswerRelevanceClassification.parse_obj(response)
+                    for response in responses
+                ]
+        except (OutputParserException, ValidationError) as err:
+            print(f"Could not parse LLM response: {[result.text for result in result.generations[0]]}")
+            print(f"Error: {err}")
+            return np.nan
+
+        return self._calculate_score(answers, row)
 
     def adapt(self, language: str, cache_dir: str | None = None) -> None:
         assert self.llm is not None, "LLM is not set"
