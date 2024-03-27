60c271da6e8cf0473ac99b9fd4a7df81f1281160
Shahules786 <Shahules786@gmail.com>
2024-03-27 15:49:45 -0700

    add custom output parser

--- src/ragas/llms/output_parser.py
+++ src/ragas/llms/output_parser.py
@@ -1,13 +1,27 @@
-import typing as t
 import json
+import typing as t
 
+from langchain_core.exceptions import OutputParserException
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.pydantic_v1 import BaseModel
+
+from ragas.llms import BaseRagasLLM
+from ragas.llms.prompt import Prompt, PromptValue
 
 # The get_format_instructions function is a modified version from
 # langchain_core.output_parser.pydantic. The original version removed the "type" json schema
 # property that confused some older LLMs.
 
 TBaseModel = t.TypeVar("TBaseModel", bound=BaseModel)
+
+FIX_OUTPUT_FORMAT = Prompt(
+    name="",
+    instruction="Below, the Completion did not satisfy the constraints given in the Prompt.",
+    output_format_instruction="",
+    input_keys=["prompt", "completion"],
+    output_key="fixed_completion",
+)
+
 
 JSON_FORMAT_INSTRUCTIONS = """The output should be a well-formatted JSON instance that conforms to the JSON schema below.
 
@@ -35,3 +49,22 @@ def get_json_format_instructions(pydantic_object: t.Type[TBaseModel]) -> str:
 
     resp = JSON_FORMAT_INSTRUCTIONS.format(schema=schema_str)
     return resp
+
+
+class RagasoutputParser(PydanticOutputParser):
+    async def aparse(
+        self, result: str, prompt: PromptValue, llm: BaseRagasLLM, max_retries: int
+    ):
+        try:
+            output = super().parse(result)
+        except OutputParserException:
+            if max_retries != 0:
+                p_value = FIX_OUTPUT_FORMAT.format(
+                    prompt=prompt.to_string(), completion=result
+                )
+                output = await llm.generate(p_value)
+                result = output.generations[0][0].text
+                return await self.aparse(result, prompt, llm, max_retries - 1)
+            else:
+                return None
+        return output
