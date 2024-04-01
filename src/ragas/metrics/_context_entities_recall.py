a2886564305fae6d254ea36c9adbb95504350c2a
Janos Tolgyesi <janos.tolgyesi@gmail.com>
2024-04-01 21:50:11 +0200

    More robust JSON prompting and parsing (#807)
    This PR implements a langchain-style, hopefully more robust output
    parsing as discussed in #761 .
    
    ---------
    
    Co-authored-by: Shahules786 <Shahules786@gmail.com>
    Co-authored-by: Felix Rothe <felix.rothe@betasystems.com>
    Co-authored-by: Sebastian Modlich <smodlich@users.noreply.github.com>
    Co-authored-by: Sebastian Modlich <sebastian.modlich@telekom.de>
    Co-authored-by: Wang Jian <nofrish@gmail.com>
    Co-authored-by: Daniel Camejo <71190106+dcamejo1@users.noreply.github.com>

--- src/ragas/metrics/_context_entities_recall.py
+++ src/ragas/metrics/_context_entities_recall.py
@@ -5,7 +5,10 @@ import typing as t
 from dataclasses import dataclass, field
 from typing import Dict
 
-from ragas.llms.json_load import json_loader
+import numpy as np
+from langchain.pydantic_v1 import BaseModel
+
+from ragas.llms.output_parser import RagasoutputParser, get_json_format_instructions
 from ragas.llms.prompt import Prompt
 from ragas.metrics.base import EvaluationMode, MetricWithLLM
 
@@ -14,64 +17,90 @@ if t.TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
+
+class ContextEntitiesResponse(BaseModel):
+    entities: t.List[str]
+
+
+_output_instructions = get_json_format_instructions(
+    pydantic_object=ContextEntitiesResponse
+)
+_output_parser = RagasoutputParser(pydantic_object=ContextEntitiesResponse)
+
+
 TEXT_ENTITY_EXTRACTION = Prompt(
     name="text_entity_extraction",
     instruction="""Given a text, extract unique entities without repetition. Ensure you consider different forms or mentions of the same entity as a single entity.""",
     input_keys=["text"],
     output_key="output",
     output_type="json",
+    output_format_instruction=_output_instructions,
     examples=[
         {
-            "text": """The Eiffel Tower, located in Paris, France, is one of the most iconic landmarks globally. 
-            Millions of visitors are attracted to it each year for its breathtaking views of the city. 
+            "text": """The Eiffel Tower, located in Paris, France, is one of the most iconic landmarks globally.
+            Millions of visitors are attracted to it each year for its breathtaking views of the city.
             Completed in 1889, it was constructed in time for the 1889 World's Fair.""",
-            "output": {
-                "entities": ["Eiffel Tower", "Paris", "France", "1889", "World's Fair"],
-            },
+            "output": ContextEntitiesResponse.parse_obj(
+                {
+                    "entities": [
+                        "Eiffel Tower",
+                        "Paris",
+                        "France",
+                        "1889",
+                        "World's Fair",
+                    ],
+                }
+            ).dict(),
         },
         {
-            "text": """The Colosseum in Rome, also known as the Flavian Amphitheatre, stands as a monument to Roman architectural and engineering achievement. 
-            Construction began under Emperor Vespasian in AD 70 and was completed by his son Titus in AD 80. 
+            "text": """The Colosseum in Rome, also known as the Flavian Amphitheatre, stands as a monument to Roman architectural and engineering achievement.
+            Construction began under Emperor Vespasian in AD 70 and was completed by his son Titus in AD 80.
             It could hold between 50,000 and 80,000 spectators who watched gladiatorial contests and public spectacles.""",
-            "output": {
-                "entities": [
-                    "Colosseum",
-                    "Rome",
-                    "Flavian Amphitheatre",
-                    "Vespasian",
-                    "AD 70",
-                    "Titus",
-                    "AD 80",
-                ],
-            },
+            "output": ContextEntitiesResponse.parse_obj(
+                {
+                    "entities": [
+                        "Colosseum",
+                        "Rome",
+                        "Flavian Amphitheatre",
+                        "Vespasian",
+                        "AD 70",
+                        "Titus",
+                        "AD 80",
+                    ],
+                }
+            ).dict(),
         },
         {
-            "text": """The Great Wall of China, stretching over 21,196 kilometers from east to west, is a marvel of ancient defensive architecture. 
-            Built to protect against invasions from the north, its construction started as early as the 7th century BC. 
+            "text": """The Great Wall of China, stretching over 21,196 kilometers from east to west, is a marvel of ancient defensive architecture.
+            Built to protect against invasions from the north, its construction started as early as the 7th century BC.
             Today, it is a UNESCO World Heritage Site and a major tourist attraction.""",
-            "output": {
-                "entities": [
-                    "Great Wall of China",
-                    "21,196 kilometers",
-                    "7th century BC",
-                    "UNESCO World Heritage Site",
-                ],
-            },
+            "output": ContextEntitiesResponse.parse_obj(
+                {
+                    "entities": [
+                        "Great Wall of China",
+                        "21,196 kilometers",
+                        "7th century BC",
+                        "UNESCO World Heritage Site",
+                    ],
+                }
+            ).dict(),
         },
         {
-            "text": """The Apollo 11 mission, which launched on July 16, 1969, marked the first time humans landed on the Moon. 
-            Astronauts Neil Armstrong, Buzz Aldrin, and Michael Collins made history, with Armstrong being the first man to step on the lunar surface. 
+            "text": """The Apollo 11 mission, which launched on July 16, 1969, marked the first time humans landed on the Moon.
+            Astronauts Neil Armstrong, Buzz Aldrin, and Michael Collins made history, with Armstrong being the first man to step on the lunar surface.
             This event was a significant milestone in space exploration.""",
-            "output": {
-                "entities": [
-                    "Apollo 11 mission",
-                    "July 16, 1969",
-                    "Moon",
-                    "Neil Armstrong",
-                    "Buzz Aldrin",
-                    "Michael Collins",
-                ],
-            },
+            "output": ContextEntitiesResponse.parse_obj(
+                {
+                    "entities": [
+                        "Apollo 11 mission",
+                        "July 16, 1969",
+                        "Moon",
+                        "Neil Armstrong",
+                        "Buzz Aldrin",
+                        "Michael Collins",
+                    ],
+                }
+            ).dict(),
         },
     ],
 )
@@ -108,7 +137,7 @@ class ContextEntityRecall(MetricWithLLM):
     batch_size: int = 15
 
     def _compute_score(
-        self, ground_truth_entities: set, context_entities: set
+        self, ground_truth_entities: t.Sequence[str], context_entities: t.Sequence[str]
     ) -> float:
         num_entities_in_both = len(
             set(context_entities).intersection(set(ground_truth_entities))
@@ -120,7 +149,7 @@ class ContextEntityRecall(MetricWithLLM):
         text: str,
         callbacks: Callbacks,
         is_async: bool,
-    ) -> Dict:
+    ) -> t.Optional[ContextEntitiesResponse]:
         assert self.llm is not None, "LLM is not initialized"
 
         result = await self.llm.generate(
@@ -130,11 +159,13 @@ class ContextEntityRecall(MetricWithLLM):
             callbacks=callbacks,
             is_async=is_async,
         )
-        response_dict = await json_loader.safe_load(
-            result.generations[0][0].text, self.llm, is_async=is_async
-        )
-        response_dict = response_dict if isinstance(response_dict, dict) else {}
-        return response_dict
+
+        result_text = result.generations[0][0].text
+        answer = _output_parser.parse(result_text)
+        if answer is None:
+            return ContextEntitiesResponse(entities=[])
+
+        return answer
 
     async def _ascore(
         self,
@@ -149,9 +180,9 @@ class ContextEntityRecall(MetricWithLLM):
         contexts = await self.get_entities(
             "\n".join(contexts), callbacks=callbacks, is_async=is_async
         )
-        return self._compute_score(
-            ground_truth.get("entities", []), contexts.get("entities", [])
-        )
+        if ground_truth is None or contexts is None:
+            return np.nan
+        return self._compute_score(ground_truth.entities, contexts.entities)
 
     def save(self, cache_dir: str | None = None) -> None:
         return self.context_entity_recall_prompt.save(cache_dir)
--- src/ragas/metrics/_faithfulness.py
+++ src/ragas/metrics/_faithfulness.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+import json
 import logging
 import typing as t
 from dataclasses import dataclass, field
 
 import numpy as np
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
 
-from ragas.llms.json_load import json_loader
+from ragas.llms.output_parser import RagasoutputParser, get_json_format_instructions
 from ragas.llms.prompt import Prompt
 from ragas.metrics.base import EvaluationMode, MetricWithLLM
 
@@ -17,90 +20,130 @@ if t.TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
+
+class StatementsAnswers(BaseModel):
+    __root__: t.List[str] = Field(..., description="the list of extracted statements")
+
+    def dicts(self) -> t.List[t.Dict]:
+        return self.dict()["__root__"]
+
+
+_statements_output_instructions = get_json_format_instructions(StatementsAnswers)
+_statements_output_parser = RagasoutputParser(pydantic_object=StatementsAnswers)
+
+
 LONG_FORM_ANSWER_PROMPT = Prompt(
     name="long_form_answer",
     instruction="Create one or more statements from each sentence in the given answer.",
+    output_format_instruction=_statements_output_instructions,
     examples=[
         {
             "question": "Who was  Albert Einstein and what is he best known for?",
             "answer": "He was a German-born theoretical physicist, widely acknowledged to be one of the greatest and most influential physicists of all time. He was best known for developing the theory of relativity, he also made important contributions to the development of the theory of quantum mechanics.",
-            "statements": {
-                "statements": [
+            "statements": StatementsAnswers.parse_obj(
+                [
                     "Albert Einstein, a German-born theoretical physicist, is renowned for being one of the most influential physicists in history.",
                     "Albert Einstein was best known for his theory of relativity.",
                     "Einstein's contributions significantly advanced the field of quantum mechanics",
                     "Recognized globally, Einstein's work has profoundly impacted the scientific community",
                     "Einstein's groundbreaking theories continue to shape our understanding of physics today.",
                 ]
-            },
+            ).dicts(),
         },
         {
             "question": "Cadmium Chloride is slightly soluble in this chemical, it is also called what?",
             "answer": "alcohol",
-            "statements": {
-                "statements": ["Cadmium Chloride is slightly soluble in alcohol."]
-            },
+            "statements": StatementsAnswers.parse_obj(
+                ["Cadmium Chloride is slightly soluble in alcohol."]
+            ).dicts(),
         },
         {
             "question": "Were Hitler and Benito Mussolini of the same nationality?",
             "answer": "Sorry, I can't provide answer to that question.",
-            "statements": {"statements": []},
+            "statements": StatementsAnswers.parse_obj([]).dicts(),
         },
     ],
     input_keys=["question", "answer"],
     output_key="statements",
-    output_type="JSON",
+    output_type="json",
 )  # noqa: E501
 
 
+class StatementFaithfulnessAnswer(BaseModel):
+    statement: str = Field(..., description="the original statement, word-by-word")
+    verdict: int = Field(..., description="the verdict(0/1) of the faithfulness.")
+    reason: str = Field(..., description="the reason of the verdict")
+
+
+class StatementFaithfulnessAnswers(BaseModel):
+    __root__: t.List[StatementFaithfulnessAnswer]
+
+    def dicts(self) -> t.List[t.Dict]:
+        return self.dict()["__root__"]
+
+
+_faithfulness_output_instructions = get_json_format_instructions(
+    StatementFaithfulnessAnswers
+)
+_faithfulness_output_parser = PydanticOutputParser(
+    pydantic_object=StatementFaithfulnessAnswers
+)
+
 NLI_STATEMENTS_MESSAGE = Prompt(
     name="nli_statements",
-    instruction="Natural language inference. Use only 'Yes' (1), 'No' (0)",
+    instruction="Your task is to judge the faithfulness of a series of statements based on a given context. For each statement you must return verdict as 1 if the statement can be verified based on the context or 0 if the statement can not be verified based on the context.",
+    output_format_instruction=_faithfulness_output_instructions,
     examples=[
         {
             "context": """John is a student at XYZ University. He is pursuing a degree in Computer Science. He is enrolled in several courses this semester, including Data Structures, Algorithms, and Database Management. John is a diligent student and spends a significant amount of time studying and completing assignments. He often stays late in the library to work on his projects.""",
-            "statements": """
-            statement_1: John is majoring in Biology.
-            statement_2: John is taking a course on Artificial Intelligence.
-            statement_3: John is a dedicated student.
-            statement_4: John has a part-time job.
-            """,
-            "answer": [
-                {
-                    "statement_1": "John is majoring in Biology.",
-                    "reason": "John's major is explicitly mentioned as Computer Science. There is no information suggesting he is majoring in Biology.",
-                    "verdict": "0",
-                },
-                {
-                    "statement_2": "John is taking a course on Artificial Intelligence.",
-                    "reason": "The context mentions the courses John is currently enrolled in, and Artificial Intelligence is not mentioned. Therefore, it cannot be deduced that John is taking a course on AI.",
-                    "verdict": "0",
-                },
-                {
-                    "statement_3": "John is a dedicated student.",
-                    "reason": "The context states that he spends a significant amount of time studying and completing assignments. Additionally, it mentions that he often stays late in the library to work on his projects, which implies dedication.",
-                    "verdict": "1",
-                },
-                {
-                    "statement_4": "John has a part-time job.",
-                    "reason": "There is no information given in the context about John having a part-time job.",
-                    "verdict": "0",
-                },
+            "statements": [
+                "John is majoring in Biology.",
+                "John is taking a course on Artificial Intelligence.",
+                "John is a dedicated student.",
+                "John has a part-time job.",
             ],
+            "answer": StatementFaithfulnessAnswers.parse_obj(
+                [
+                    {
+                        "statement": "John is majoring in Biology.",
+                        "reason": "John's major is explicitly mentioned as Computer Science. There is no information suggesting he is majoring in Biology.",
+                        "verdict": 0,
+                    },
+                    {
+                        "statement": "John is taking a course on Artificial Intelligence.",
+                        "reason": "The context mentions the courses John is currently enrolled in, and Artificial Intelligence is not mentioned. Therefore, it cannot be deduced that John is taking a course on AI.",
+                        "verdict": 0,
+                    },
+                    {
+                        "statement": "John is a dedicated student.",
+                        "reason": "The context states that he spends a significant amount of time studying and completing assignments. Additionally, it mentions that he often stays late in the library to work on his projects, which implies dedication.",
+                        "verdict": 1,
+                    },
+                    {
+                        "statement": "John has a part-time job.",
+                        "reason": "There is no information given in the context about John having a part-time job.",
+                        "verdict": 0,
+                    },
+                ]
+            ).dicts(),
         },
         {
             "context": """Photosynthesis is a process used by plants, algae, and certain bacteria to convert light energy into chemical energy.""",
-            "statements": """statement_1: Albert Einstein was a genius.""",
-            "answer": {
-                "statement_1": "Albert Einstein was a genius.",
-                "reason": "The context and statement are unrelated",
-                "verdict": "0",
-            },
+            "statements": ["Albert Einstein was a genius."],
+            "answer": StatementFaithfulnessAnswers.parse_obj(
+                [
+                    {
+                        "statement": "Albert Einstein was a genius.",
+                        "reason": "The context and statement are unrelated",
+                        "verdict": 0,
+                    },
+                ]
+            ).dicts(),
         },
     ],
     input_keys=["context", "statements"],
     output_key="answer",
-    output_type="JSON",
+    output_type="json",
 )  # noqa: E501
 
 
@@ -124,39 +167,28 @@ class Faithfulness(MetricWithLLM):
         )
         return prompt_value
 
-    def _create_nli_prompt(self, row: t.Dict, statements: t.Any) -> PromptValue:
+    def _create_nli_prompt(self, row: t.Dict, statements: t.List[str]) -> PromptValue:
         assert self.llm is not None, "llm must be set to compute score"
 
         contexts = row["contexts"]
         # check if the statements are support in the contexts
         contexts_str: str = "\n".join(contexts)
-        statements_str: str = "\n".join(
-            [f"statement_{i+1}: {st}" for i, st in enumerate(statements)]
-        )
+        statements_str: str = json.dumps(statements)
         prompt_value = self.nli_statements_message.format(
             context=contexts_str, statements=statements_str
         )
         return prompt_value
 
-    def _compute_score(self, output: t.Any):
+    def _compute_score(self, answers: StatementFaithfulnessAnswers):
         # check the verdicts and compute the score
-        verdict_score_map = {"1": 1, "0": 0}
-        output = output if isinstance(output, list) else [output]
         faithful_statements = sum(
-            verdict_score_map.get(
-                str(statement_with_validation.get("verdict", "")), np.nan
-            )
-            if isinstance(statement_with_validation, dict)
-            else np.nan
-            for statement_with_validation in output
+            1 if answer.verdict else 0 for answer in answers.__root__
         )
-        num_statements = len(output)
+        num_statements = len(answers.__root__)
         if num_statements:
             score = faithful_statements / num_statements
         else:
-            logger.warning(
-                "Invalid JSON response. Expected dictionary with key 'verdict'"
-            )
+            logger.warning("No statements were generated from the answer.")
             score = np.nan
 
         return score
@@ -172,29 +204,20 @@ class Faithfulness(MetricWithLLM):
         answer_result = await self.llm.generate(
             p, callbacks=callbacks, is_async=is_async
         )
-        statements = await json_loader.safe_load(
-            text=answer_result.generations[0][0].text,
-            llm=self.llm,
-            callbacks=callbacks,
-            is_async=is_async,
-        )
+        answer_result_text = answer_result.generations[0][0].text
+        statements = _statements_output_parser.parse(answer_result_text)
+        if statements is None:
+            return np.nan
 
-        statements = statements if isinstance(statements, dict) else {}
-        statements = statements.get("statements", [])
-        if statements:
-            p = self._create_nli_prompt(row, statements)
-            nli_result = await self.llm.generate(
-                p, callbacks=callbacks, is_async=is_async
-            )
-            json_output = await json_loader.safe_load(
-                text=nli_result.generations[0][0].text,
-                llm=self.llm,
-                callbacks=callbacks,
-                is_async=is_async,
-            )
-        else:
-            json_output = [{}]
-        return self._compute_score(json_output)
+        p = self._create_nli_prompt(row, statements.__root__)
+        nli_result = await self.llm.generate(p, callbacks=callbacks, is_async=is_async)
+        nli_result_text = nli_result.generations[0][0].text
+
+        faithfulness = _faithfulness_output_parser.parse(nli_result_text)
+        if faithfulness is None:
+            return np.nan
+
+        return self._compute_score(faithfulness)
 
     def adapt(self, language: str, cache_dir: t.Optional[str] = None) -> None:
         assert self.llm is not None, "LLM is not set"
