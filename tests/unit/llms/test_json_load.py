9a76d4106e5b6e8ddd62f117d1f6dfa93cc3e9d3
Philippe Berger <philippe.berger@hingehealth.com>
2024-04-13 15:20:44 -0400

    try with higher abstraction

--- tests/unit/llms/test_json_load.py
+++ tests/unit/llms/test_json_load.py
@@ -1,37 +1,35 @@
-import types
 import os
+import unittest
 import unittest.mock
+from typing import Any, List, Optional
+import typing as t
 
-from openai.types.chat import ChatCompletionMessage
-from openai.types.chat.chat_completion import ChatCompletion, Choice
+from langchain_core.callbacks import Callbacks, CallbackManagerForLLMRun
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages import AIMessage, BaseMessage
+from langchain_core.outputs import ChatGeneration, ChatResult, LLMResult
 
-from ragas.llms.base import llm_factory
+from ragas.llms.base import llm_factory, BaseRagasLLM
 from ragas.llms.json_load import json_loader
+from ragas.llms.prompt import PromptValue
 
 
-class MockOpenAI:
-    class MockCompletions:
-        def create(self, *args, **kwargs):
-            return ChatCompletion(
-                id="123456",
-                model="model",
-                object="chat.completion",
-                choices=[
-                    Choice(
-                        finish_reason="stop",
-                        index=0,
-                        message=ChatCompletionMessage(
-                            content='{"one": "two"},,, {"three": "four"}]',
-                            role="assistant",
-                        ),
-                    )
-                ],
-                created=123,
-            )
-
-    def __init__(self, *args, **kwargs):
-        self.chat = types.SimpleNamespace()
-        self.chat.completions = self.MockCompletions()
+class MockRagasLLM(BaseRagasLLM):
+    def generate_text(
+        self,
+        prompt: PromptValue,
+        n: int = 1,
+        temperature: float = 1e-8,
+        stop: t.Optional[t.List[str]] = None,
+        callbacks: t.Optional[Callbacks] = None,
+    ) -> LLMResult:
+        return LLMResult(
+            generations=[
+                ChatGeneration(
+                    message=AIMessage('{"one": "two"},,, {"three": "four"}]')
+                )
+            ]
+        )
 
 
 class TestJsonLoader(unittest.TestCase):
@@ -40,11 +38,12 @@ class TestJsonLoader(unittest.TestCase):
         self.mock_environment = {
             "OPENAI_API_KEY": "abcdefg123456v",
         }
+
         self.patches = [
             unittest.mock.patch.dict(os.environ, self.mock_environment),
             unittest.mock.patch(
-                "openai.OpenAI",
-                new=MockOpenAI,
+                "ragas.llms.LangchainLLMWrapper",
+                new=MockRagasLLM,
             ),
         ]
 
