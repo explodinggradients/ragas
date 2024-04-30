3606d10af41f6c25a90455385702a765e5260d5f
Shahules786 <Shahules786@gmail.com>
2024-04-30 15:13:55 -0700

    add new prompt and ensembler

--- src/ragas/metrics/base.py
+++ src/ragas/metrics/base.py
@@ -156,3 +156,29 @@ class MetricWithEmbeddings(Metric):
                 f"Metric '{self.name}' has no valid embeddings provided (self.embeddings is None). Please initantiate a the metric with an embeddings to run."  # noqa
             )
         self.embeddings.set_run_config(run_config)
+
+class Ensember:
+    def from_discrete(self, inputs: list[list[t.Dict]], attribute: str):
+        assert all(
+            len(item) == len(inputs[0]) for item in inputs
+        ), "all inputs must have the same length"
+
+        assert all(
+            attribute in item for input in inputs for item in input
+        ), "attribute not found in all items"
+
+        if len(inputs) == 1:
+            return inputs[0]
+
+        verdict_agg = []
+        for i in range(len(inputs[0])):
+            item = inputs[0][i]
+            verdicts = [inputs[k][i][attribute] for k in range(len(inputs))]
+            verdict_counts = dict(Counter(verdicts).most_common())
+            item[attribute] = list(verdict_counts.keys())[0]
+            verdict_agg.append(item)
+
+        return verdict_agg
+
+
+ensembler = Ensember()
