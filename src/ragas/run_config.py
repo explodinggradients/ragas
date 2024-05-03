02f40b062ebe53502a843362c0f6e1f41c3646ec
Jithin James <jamesjithin97@gmail.com>
2024-05-03 12:58:58 -0700

    Fix/ci: fix the broken CI (#933)

--- src/ragas/run_config.py
+++ src/ragas/run_config.py
@@ -1,3 +1,4 @@
+import logging
 import typing as t
 from dataclasses import dataclass
 
@@ -5,10 +6,12 @@ from tenacity import (
     AsyncRetrying,
     Retrying,
     WrappedFn,
+    after_log,
     retry_if_exception_type,
     stop_after_attempt,
     wait_random_exponential,
 )
+from tenacity.after import after_nothing
 
 
 @dataclass
@@ -24,15 +27,24 @@ class RunConfig:
     exception_types: t.Union[
         t.Type[BaseException],
         t.Tuple[t.Type[BaseException], ...],
-    ] = Exception
+    ] = (Exception,)
+    log_tenacity: bool = False
 
 
 def add_retry(fn: WrappedFn, run_config: RunConfig) -> WrappedFn:
+    # configure tenacity's after section wtih logger
+    if run_config.log_tenacity is not None:
+        logger = logging.getLogger(f"ragas.retry.{fn.__name__}")
+        tenacity_logger = after_log(logger, logging.DEBUG)
+    else:
+        tenacity_logger = after_nothing
+
     r = Retrying(
         wait=wait_random_exponential(multiplier=1, max=run_config.max_wait),
         stop=stop_after_attempt(run_config.max_retries),
         retry=retry_if_exception_type(run_config.exception_types),
         reraise=True,
+        after=tenacity_logger,
     )
     return r.wraps(fn)
 
@@ -41,9 +53,18 @@ def add_async_retry(fn: WrappedFn, run_config: RunConfig) -> WrappedFn:
     """
     Decorator for retrying a function if it fails.
     """
+    # configure tenacity's after section wtih logger
+    if run_config.log_tenacity is not None:
+        logger = logging.getLogger(f"TENACITYRetry[{fn.__name__}]")
+        tenacity_logger = after_log(logger, logging.DEBUG)
+    else:
+        tenacity_logger = after_nothing
+
     r = AsyncRetrying(
         wait=wait_random_exponential(multiplier=1, max=run_config.max_wait),
         stop=stop_after_attempt(run_config.max_retries),
+        retry=retry_if_exception_type(run_config.exception_types),
         reraise=True,
+        after=tenacity_logger,
     )
     return r.wraps(fn)
