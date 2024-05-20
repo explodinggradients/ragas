5aefa97a1c04f0f90d79aca18a3c8a95d3e59899
jjmachan <jamesjithin97@gmail.com>
2024-05-20 19:41:42 +0530

    added test in ci

--- tests/e2e/test_amnesty_in_ci.py
+++ tests/e2e/test_amnesty_in_ci.py
@@ -13,6 +13,13 @@ from ragas.metrics import (
 amnesty_qa = load_dataset("explodinggradients/amnesty_qa", "english_v2")["eval"]
 
 
+def assert_in_range(score: float, value: float, max_range: float):
+    """
+    Check if computed score is within the range of value +/- max_range
+    """
+    assert value - max_range <= score <= value + max_range
+
+
 @pytest.mark.ragas_ci
 def test_amnesty_e2e():
     result = evaluate(
@@ -20,5 +27,7 @@ def test_amnesty_e2e():
         metrics=[answer_relevancy, faithfulness, context_recall, context_precision],
         in_ci=True,
     )
-    print(result)
-    assert False
+    assert result["answer_relevancy"] >= 0.9
+    assert result["context_recall"] >= 0.95
+    assert result["context_precision"] >= 0.95
+    assert_in_range(result["faithfulness"], value=0.4, max_range=0.1)
