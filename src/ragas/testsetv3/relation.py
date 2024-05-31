235054facfb622745c27534f9ea7310e9b5ecd06
Shahules786 <Shahules786@gmail.com>
2024-05-31 20:16:54 +0530

    add similarity relation

--- src/ragas/testsetv3/relation.py
+++ src/ragas/testsetv3/relation.py
@@ -1,76 +1,74 @@
 import typing as t
-from dataclasses import dataclass
-from langchain_core.documents import Document as LCDocument
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
 import numpy as np
+from langchain_core.documents import Document as LCDocument
 
 
 @dataclass
-class RelationExtractor(ABC):
+class Similarity(ABC):
     name: str
     attribute1: str
     attribute2: str
-    
+
     def get_attribute(self, doc: LCDocument, attribute: str):
-        
         if hasattr(doc, self.attribute1):
             return getattr(doc, attribute)
         elif attribute in doc.metadata:
             return doc.metadata[attribute]
         else:
             return None
-        
+
     @abstractmethod
-    def extract(self, doc1: LCDocument, doc2: LCDocument) -> t.Any:
+    def extract(self, doc1: t.List[LCDocument], doc2: t.List[LCDocument]) -> t.Any:
         pass
-    
-    
-@dataclass
-class Jaccardsimilarity(RelationExtractor):
 
-    def extract(self, doc1: LCDocument, doc2: LCDocument): 
-    
-        a = self.get_attribute(doc1, self.attribute1)
-        b = self.get_attribute(doc2, self.attribute2)
-        if not isinstance(a, list) or not isinstance(b, list): 
-            raise ValueError("Attributes must be lists")
-        a = set(a)
-        b = set(b)
-        return len(a.intersection(b)) / len(a.union(b))
-    
 
 @dataclass
-class EmbeddingSimilarity(RelationExtractor):
-    
-    def extract(self, doc1: LCDocument, doc2: LCDocument) -> t.Any:
-        
-        embedding1 = getattr(doc1, self.attribute1)
-        embedding2 = getattr(doc2, self.attribute2)
-        embedding1 = np.array(embedding1)
-        embedding2 = np.array(embedding2)
-        
-        dot_product = np.dot(embedding1, embedding2)
-        norm1 = np.linalg.norm(embedding1)
-        norm2 = np.linalg.norm(embedding2)
-        
-        similarity = dot_product / (norm1 * norm2)
-        
-        return similarity
-    
-    
+class Jaccard(Similarity):
+    def extract(self, doc1: t.List[LCDocument], doc2: t.List[LCDocument]):
+        jaccard_similarity_matrix = np.zeros((len(doc1), len(doc2)))
 
+        doc1_items = [self.get_attribute(doc, self.attribute1) for doc in doc1]
+        doc2_items = [self.get_attribute(doc, self.attribute2) for doc in doc2]
+        if all(isinstance(item, list) for item in doc1_items) and all(
+            isinstance(item, list) for item in doc2_items
+        ):
+            for i, a in enumerate(doc1_items):
+                for k, b in enumerate(doc2_items):
+                    intersection = len(set(a).intersection(set(b)))
+                    union = len(set(a).union(set(b)))
+                    jaccard_similarity_matrix[i][k] = intersection / union
+
+        return jaccard_similarity_matrix
+
+
+@dataclass
+class Cosine(Similarity):
+    def extract(self, doc1: t.List[LCDocument], doc2: t.List[LCDocument]) -> t.Any:
+        embeddings_1 = [getattr(doc, self.attribute1) for doc in doc1]
+        embeddings_2 = [getattr(doc, self.attribute2) for doc in doc2]
+        embeddings_1 = np.array(embeddings_1)
+        embeddings_2 = np.array(embeddings_2)
+        cosine_similarity_matrix = np.dot(embeddings_1, embeddings_2.T) / (
+            np.linalg.norm(embeddings_1) * np.linalg.norm(embeddings_2)
+        )
+        return cosine_similarity_matrix
 
 
 if __name__ == "__main__":
-    
     from langchain_core.documents import Document as LCDocument
+
     text = """
     Contact us at info@example.com or visit https://www.example.com for more information.
     Alternatively, email support@service.com or check http://service.com.
     You can also visit our second site at www.secondary-site.org or email us at secondary-info@secondary-site.org.
     """
-    
-    docs = [LCDocument(page_content=text, metadata={'headlines':['one','two']})]
-    
-    jaccard_overlap = Jaccardsimilarity(name="jaccard", attribute1="headlines", attribute2="headlines")
-    score = jaccard_overlap.extract(docs[0], docs[0])
+
+    docs = [LCDocument(page_content=text, metadata={"headlines": ["one", "two"]})]
+
+    jaccard_overlap = Jaccardsimilarity(
+        name="jaccard", attribute1="headlines", attribute2="headlines"
+    )
+    score = jaccard_overlap.extract(docs, docs)
