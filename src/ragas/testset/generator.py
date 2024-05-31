4d596bb06260223d0d3facae92c9c6e921b47327
Manav <manavmanoj99@gmail.com>
2024-05-31 18:22:58 +0530

    fix(testset): reinitialize docstore for generation with new documents

--- src/ragas/testset/generator.py
+++ src/ragas/testset/generator.py
@@ -179,11 +179,11 @@ class TestsetGenerator:
         )
 
     def init_evolution(self, evolution: Evolution) -> None:
+        # if evolution.docstore is None:
+        evolution.docstore = self.docstore
+        
         if evolution.generator_llm is None:
             evolution.generator_llm = self.generator_llm
-            if evolution.docstore is None:
-                evolution.docstore = self.docstore
-
             if evolution.question_filter is None:
                 evolution.question_filter = QuestionFilter(llm=self.critic_llm)
             if evolution.node_filter is None:
