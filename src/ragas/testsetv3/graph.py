6468d1c1f74cb63c895d073dd54e7f2f1b3e996b
Shahules786 <Shahules786@gmail.com>
2024-06-03 17:47:17 +0530

    added grapgql query

--- src/ragas/testsetv3/graph.py
+++ src/ragas/testsetv3/graph.py
@@ -1,4 +1,9 @@
-from graphene import ObjectType, String, List, Field, Int
+from graphene import ObjectType, String, List, Field, JSONString, Enum, Schema, Argument
+import uuid
+
+class NodeType(Enum):
+    DOC = "doc"
+    CHUNK = "chunk"
 
 
 class Relationship(ObjectType):
@@ -14,7 +19,7 @@ class Relationship(ObjectType):
     source = Field(lambda: Node)
     target = Field(lambda: Node)
     label = String()
-    properties = Field(lambda: dict)
+    properties = Field(JSONString)
 
 
 class Node(ObjectType):
@@ -26,7 +31,33 @@ class Node(ObjectType):
         properties (dict): Additional properties and metadata associated with the node.
     """
 
-    id = String()
-    label = String()
-    properties = Field(lambda: dict)
-    relationships = List(Relationship)
+    id = String(required=True)
+    label = Field(NodeType)
+    properties = Field(JSONString)
+    relationships = List(lambda: Relationship)
+    
+    def __init__(self, id=None, **kwargs):
+        if id is None:
+            id = str(uuid.uuid4())
+        super().__init__(id=id, **kwargs)
+
+
+class Query(ObjectType):
+    node = Field(Node, id=String(required=True))
+    nodes_by_label = List(Node, label=Argument(NodeType, required=True))
+
+    def resolve_node(parent, info, id):
+        nodes = info.context.get('nodes', [])
+        for node in nodes:
+            if node.id == id:
+                return node
+        return None
+
+    def resolve_nodes_by_label(parent, info, label):
+        nodes = info.context.get('nodes', [])
+        return [node for node in nodes if node.label == label]
+    
+    
+    
+schema = Schema(query=Query)
+    
