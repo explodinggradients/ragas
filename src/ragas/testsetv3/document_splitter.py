990f4939f4f1a2b433d7ec2ed7b37cc53b8abb52
Shahules786 <Shahules786@gmail.com>
2024-06-03 17:47:50 +0530

    document splitter outputs nodes and relations

--- src/ragas/testsetv3/document_splitter.py
+++ src/ragas/testsetv3/document_splitter.py
@@ -1,8 +1,11 @@
 import typing as t
 from dataclasses import dataclass
+
 from langchain_core.documents import Document as LCDocument
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+
+from ragas.testsetv3.graph import Node, Relationship, NodeType
 
 @dataclass
 class DocumentSplitter:
@@ -21,14 +24,16 @@ class DocumentSplitter:
         for idx, text_chunk in enumerate(text_chunks):
             text_chunk_metadata = {}
             for metadata_key in extractive_metadata_keys:
-                metadata = doc.metadata[metadata_key]
+                metadata = doc.metadata.get(metadata_key)
                 if isinstance(metadata, str):
                     idx = text_chunk.find(metadata)
                     if idx != -1:
                         text_chunk_metadata[metadata_key] = metadata
 
                 elif isinstance(metadata, list):
-                    metadata_match_idx = [text_chunk.find(item) for item in metadata]
+                    metadata_match_idx = [text_chunk.find(item
+                                                          
+                                                          ) for item in metadata]
                     metadata_idx = [
                         idx
                         for idx, match_idx in enumerate(metadata_match_idx)
@@ -39,15 +44,46 @@ class DocumentSplitter:
                             metadata[i] for i in metadata_idx
                         ]
 
-            text_chunk_doc = LCDocument(
-                page_content=text_chunk, metadata=text_chunk_metadata
+            text_chunk_doc = Node(
+                label = NodeType.CHUNK,
+                properties={"page_content":text_chunk, "metadata":text_chunk_metadata}
             )
             text_chunks_docs.append(text_chunk_doc)
 
         return text_chunks_docs
 
+    def find_seperator(self, doc: Node):
+        for separator in self.separators:
+            if doc.properties["page_content"].startswith(separator):
+                return separator
+            
     def split_documents(self, documents: t.Sequence[LCDocument]):
+        nodes = []
+        relationships = []
         for doc in documents:
             text_chunks = self.text_splitter.split_text(doc.page_content)
             text_chunks_docs = self.create_docs(text_chunks, doc)
-            return text_chunks_docs
+            doc = Node(
+                label=NodeType.DOC,
+                properties={"page_content":doc.page_content, "metadata":doc.metadata},
+                relationships=[])
+            nodes.append(doc)
+            for chunk in text_chunks_docs:
+                seperator = self.find_seperator(chunk)
+                relationship = Relationship(
+                        source=doc,
+                        target=chunk,
+                        label="contains",
+                        properties={"seperator":seperator or "None"}
+                    )
+                
+                relationships.append(relationship)
+                doc.relationships.append(relationship)
+                nodes.append(chunk)
+
+            nodes.extend(text_chunks_docs)
+            
+        return nodes, relationships
+                
+            
+            
