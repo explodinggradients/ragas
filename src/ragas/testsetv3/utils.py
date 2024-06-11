1df85804d6ed04cbd62069ea2a344aa316eae67a
Shahules786 <Shahules786@gmail.com>
2024-06-11 13:39:37 +0530

    recursive merge dicts

--- src/ragas/testsetv3/utils.py
+++ src/ragas/testsetv3/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 MODEL_MAX_LENGTHS = {
     "gpt-3.5-turbo": 16385,
 }
@@ -8,24 +7,22 @@ MODEL_MAX_LENGTHS = {
 
 rng = np.random.default_rng(seed=42)
 
+
 def merge_dicts(*dicts):
     merged_dict = {}
-    
+
     for d in dicts:
         for key, value in d.items():
             if key in merged_dict:
                 if isinstance(value, list) and isinstance(merged_dict[key], list):
                     merged_dict[key].extend(value)
                 elif isinstance(value, str) and isinstance(merged_dict[key], str):
-                    merged_dict[key] += '-' + value
+                    merged_dict[key] += "-" + value
+                elif isinstance(value, dict) and isinstance(merged_dict[key], dict):
+                    merged_dict[key] = merge_dicts(merged_dict[key], value)
                 else:
                     raise TypeError("Inconsistent value types for key: {}".format(key))
             else:
-                if isinstance(value, list):
-                    merged_dict[key] = list(value)
-                elif isinstance(value, str):
-                    merged_dict[key] = value
-                else:
-                    raise TypeError("Unsupported value type for key: {}".format(key))
-                    
+                merged_dict[key] = value
+
     return merged_dict
