50394ae711a8b5b184ef860b00d52e08e923eeb8
Shahules786 <Shahules786@gmail.com>
2024-06-16 18:59:51 +0530

    add graphene objects converter

--- src/ragas/testsetv3/utils.py
+++ src/ragas/testsetv3/utils.py
@@ -1,4 +1,8 @@
+import json
+
 import numpy as np
+
+from ragas.testsetv3.graph import Node, NodeType, Relationship, NodeLevel
 
 MODEL_MAX_LENGTHS = {
     "gpt-3.5-turbo": 16385,
@@ -26,3 +30,46 @@ def merge_dicts(*dicts):
                 merged_dict[key] = value
 
     return merged_dict
+
+
+class GraphConverter:
+    @staticmethod
+    def dict_to_node(node_dict):
+        relationships = [
+            GraphConverter.dict_to_relationship(rel_dict, node_dict["id"])
+            for rel_dict in node_dict.get("relationships", [])
+        ]
+        return Node(
+            id=node_dict["id"],
+            label=NodeType[node_dict["label"]],
+            properties=json.loads(node_dict["properties"]),
+            relationships=relationships,
+            level=NodeLevel(node_dict["level"]) if "level" in node_dict else None,
+        )
+
+    @staticmethod
+    def dict_to_relationship(rel_dict, source_id=None):
+        target_node = Node(
+            id=rel_dict["target"]["id"],
+            label=NodeType[rel_dict["target"]["label"]],
+            properties=json.loads(rel_dict["target"]["properties"]),
+        )
+        return Relationship(
+            source=Node(id=source_id) if source_id else None,
+            target=target_node,
+            label=rel_dict["label"],
+            properties=json.loads(rel_dict["properties"]),
+        )
+
+    @staticmethod
+    def convert_dict_to_object(item):
+        if "source" in item and "target" in item:
+            return GraphConverter.dict_to_relationship(item)
+        elif "id" in item and "label" in item:
+            return GraphConverter.dict_to_node(item)
+        else:
+            raise ValueError("Unknown item type")
+
+    @staticmethod
+    def convert_dicts_to_objects(data):
+        return [GraphConverter.convert_dict_to_object(item) for item in data]
