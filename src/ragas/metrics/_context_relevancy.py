fcfeeaaee1e3369497558b5150b5b939dc5cf0e7
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2024-06-19 08:42:18 +0200

    fix(typo): missing 'to' in prompt instruction

--- src/ragas/metrics/_context_relevancy.py
+++ src/ragas/metrics/_context_relevancy.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 CONTEXT_RELEVANCE = Prompt(
     name="context_relevancy",
-    instruction="""Please extract relevant sentences from the provided context that is absolutely required answer the following question. If no relevant sentences are found, or if you believe the question cannot be answered from the given context, return the phrase "Insufficient Information".  While extracting candidate sentences you're not allowed to make any changes to sentences from given context.""",
+    instruction="""Please extract relevant sentences from the provided context that is absolutely required to answer the following question. If no relevant sentences are found, or if you believe the question cannot be answered from the given context, return the phrase "Insufficient Information".  While extracting candidate sentences you're not allowed to make any changes to sentences from given context.""",
     input_keys=["question", "context"],
     output_key="candidate sentences",
     output_type="json",
