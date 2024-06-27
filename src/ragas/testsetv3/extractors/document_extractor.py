149c4600bb2d7bb6c2c6545e04275c8107777109
Shahules786 <Shahules786@gmail.com>
2024-06-27 13:13:44 +0530

    add executor and clean methods

--- src/ragas/testsetv3/extractors/document_extractor.py
+++ src/ragas/testsetv3/extractors/document_extractor.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from langchain_core.documents import Document as LCDocument
 
 from ragas.embeddings.base import BaseRagasEmbeddings, embedding_factory
+from ragas.executor import Executor
 from ragas.testsetv3.extractors.base import Extractor
 from ragas.testsetv3.extractors.llm_based import LLMbasedExtractor
 from ragas.testsetv3.extractors.regex_based import RulebasedExtractor
@@ -37,98 +38,129 @@ class DocumentExtractor:
             else None
         )
 
-    async def extract_from_documents(self, documents: t.Sequence[LCDocument]):
-        for doc in documents:
-            if self.llm_extractors:
-                for llm_ext in self.llm_extractors:
-                    output = await llm_ext.extract(doc.page_content)
-                    doc.metadata.update(output)
-            if self.regex_extractors:
-                for reg_ext in self.regex_extractors:
-                    output = reg_ext.extract(doc.page_content)
-                    doc.metadata.update(output)
+    async def extract(
+        self, inputs: t.Union[t.Sequence[Node], t.Sequence[LCDocument]], **args
+    ) -> t.Union[t.Sequence[Node], t.Sequence[LCDocument]]:
+        exec = Executor(
+            desc="Extraction..",
+            keep_progress_bar=True,
+            raise_exceptions=True,
+            run_config=None,
+        )
 
-        doc = documents[0]
+        if all(isinstance(item, Node) for item in inputs):
+            for item in inputs:
+                exec.submit(self._extract_from_node, item, **args)
+        elif all(isinstance(item, LCDocument) for item in inputs):
+            for item in inputs:
+                exec.submit(self._extract_from_document, item)
+        else:
+            raise ValueError("Input must be a list of Nodes or Documents")
+
+        return exec.results()
+
+    async def embed(
+        self,
+        inputs: t.Union[t.Sequence[Node], t.Sequence[LCDocument]],
+        attributes: t.List[str],
+        **args,
+    ) -> t.Union[t.Sequence[Node], t.Sequence[LCDocument]]:
+        exec = Executor(
+            desc="Embeddding..",
+            keep_progress_bar=True,
+            raise_exceptions=True,
+            run_config=None,
+        )
+
+        if all(isinstance(item, Node) for item in inputs):
+            for item in inputs:
+                exec.submit(self._embed_from_node, item, attributes, **args)
+        elif all(isinstance(item, LCDocument) for item in inputs):
+            for item in inputs:
+                exec.submit(self._embed_document, item, attributes)
+        else:
+            raise ValueError("Input must be a list of Nodes or Documents")
+
+        return exec.results()
+
+    async def _extract_from_document(self, doc: LCDocument) -> LCDocument:
+        if self.llm_extractors:
+            for llm_ext in self.llm_extractors:
+                output = await llm_ext.aextract(doc)
+                doc.metadata.update(output)
+        if self.regex_extractors:
+            for reg_ext in self.regex_extractors:
+                output = reg_ext.extract(doc)
+                doc.metadata.update(output)
+
         extractive_metadata_keys = []
         for metadata in doc.metadata:
             if isinstance(doc.metadata[metadata], str):
                 idx = doc.page_content.find(doc.metadata[metadata])
                 if idx != -1:
                     extractive_metadata_keys.append(metadata)
-            elif isinstance(doc.metadata[metadata], list):
+            elif isinstance(doc.metadata[metadata], list) and all(
+                isinstance(item, str) for item in doc.metadata[metadata]
+            ):
                 idx = [doc.page_content.find(item) for item in doc.metadata[metadata]]
                 if sum(i != -1 for i in idx) > len(idx) / 2:
                     extractive_metadata_keys.append(metadata)
 
-        for doc in documents:
-            doc.metadata["extractive_metadata_keys"] = extractive_metadata_keys
+        doc.metadata["extractive_metadata_keys"] = extractive_metadata_keys
 
-        return documents
+        return doc
 
-    async def extract_from_nodes(
-        self, nodes: t.List[Node], levels: t.Union[str, t.List[NodeLevel]]
-    ):
-        for node in nodes:
-            if node.level in levels or levels == "any":
-                if self.llm_extractors:
-                    for llm_ext in self.llm_extractors:
-                        output = await llm_ext.extract(node.properties["page_content"])
-                        node.properties["metadata"].update(output)
-                if self.regex_extractors:
-                    for reg_ext in self.regex_extractors:
-                        output = reg_ext.extract(node.properties["page_content"])
-                        node.properties["metadata"].update(output)
+    async def _extract_from_node(
+        self, node: Node, levels: t.Union[str, t.List[NodeLevel]] = "any"
+    ) -> Node:
+        if node.level in levels or levels == "any":
+            if self.llm_extractors:
+                for llm_ext in self.llm_extractors:
+                    output = await llm_ext.aextract_from_node(node)
+                    node.properties["metadata"].update(output)
+            if self.regex_extractors:
+                for reg_ext in self.regex_extractors:
+                    output = reg_ext.extract_from_node(node)
+                    node.properties["metadata"].update(output)
 
-        return nodes
+        return node
 
-    async def embed_from_documents(
-        self, documents: t.Sequence[LCDocument], attributes: t.List[str]
-    ):
+    async def _embed_document(
+        self, doc: LCDocument, attributes: t.List[str]
+    ) -> LCDocument:
         self.embedding = (
             self.embedding if self.embedding is not None else embedding_factory()
         )
         for attr in attributes:
             if attr == "page_content":
-                items_to_embed = [doc.page_content for doc in documents]
-            elif attr in documents[0].metadata:
-                items_to_embed = [doc.metadata.get(attr, "") for doc in documents]
+                item_to_embed = doc.page_content
+            elif attr in doc.metadata:
+                item_to_embed = doc.metadata.get(attr, "")
             else:
                 raise ValueError(f"Attribute {attr} not found in document")
 
-            embeddings_list = await self.embedding.aembed_documents(items_to_embed)
-            assert len(embeddings_list) == len(
-                items_to_embed
-            ), "Embeddings and document must be of equal length"
-            for doc, embedding in zip(documents, embeddings_list):
-                doc.metadata[f"{attr}_embedding"] = embedding
+            embedding = await self.embedding.aembed_query(item_to_embed)
+            doc.metadata[f"{attr}_embedding"] = embedding
 
-        return documents
+        return doc
 
-    async def embed_from_nodes(
+    async def _embed_from_node(
         self,
-        nodes: t.List[Node],
+        node: Node,
         attributes: t.List[str],
-        levels: t.Union[str, t.List[NodeLevel]],
-    ):
-        if levels == "any":
-            nodes_ = nodes
-        else:
-            nodes_ = [node for node in nodes if node.level in levels]
-        self.embedding = (
-            self.embedding if self.embedding is not None else embedding_factory()
-        )
-        nodes_ = [node for node in nodes if node.level in levels]
-        for attr in attributes:
-            if attr == "page_content":
-                items_to_embed = [node.properties["page_content"] for node in nodes_]
-            else:
-                items_to_embed = [node.properties["metadata"][attr] for node in nodes_]
+        levels: t.Union[str, t.List[NodeLevel]] = "any",
+    ) -> Node:
+        if levels == "any" or (isinstance(levels, list) and node.level in levels):
+            self.embedding = (
+                self.embedding if self.embedding is not None else embedding_factory()
+            )
+            for attr in attributes:
+                if attr == "page_content":
+                    item_to_embed = node.properties["page_content"]
+                else:
+                    item_to_embed = node.properties["metadata"][attr]
 
-            embeddings_list = await self.embedding.aembed_documents(items_to_embed)
-            assert len(embeddings_list) == len(
-                items_to_embed
-            ), "Embeddings and document must be of equal length"
-            for node, embedding in zip(nodes_, embeddings_list):
+                embedding = await self.embedding.aembed_documents(item_to_embed)
                 node.properties["metadata"][f"{attr}_embedding"] = embedding
 
-        return nodes
+        return node
