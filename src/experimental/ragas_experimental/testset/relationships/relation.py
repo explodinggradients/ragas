b218cbae9e10d094a1280324ca4df253f6d6437c
Shahules786 <Shahules786@gmail.com>
2024-07-03 13:01:14 +0530

    add warning

--- src/experimental/ragas_experimental/testset/relationships/relation.py
+++ src/experimental/ragas_experimental/testset/relationships/relation.py
@@ -1,8 +1,11 @@
+import logging
 import typing as t
 from dataclasses import dataclass
 
 from ragas_experimental.testset.graph import Node, NodeLevel, Relationship, schema
 from ragas_experimental.testset.relationships.base import Similarity
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -27,21 +30,20 @@ class RelationshipBuilder:
                 }}
             }}
                 """.format(
-            node_level=node_level
+            node_level=node_level.name
         )
-
         results = schema.execute(
             query, context={"nodes": nodes, "relationships": relationships}
         )
 
         if results.errors:
             raise Exception(results.errors)
-        if results.data is None:
-            return None
+        if not results.data.get("filterNodes"):
+            logger.warning("No new relations created due to empty results.")
+            return (nodes, relationships)
 
         node_ids = [item.get("id") for item in results.data["filterNodes"]]
         nodes_ = [node for node in nodes if node.id in node_ids]
-
         for extractor in similarity_functions:
             similarity_matrix = extractor.extract(nodes_, nodes_)
             for i, row in enumerate(similarity_matrix):
