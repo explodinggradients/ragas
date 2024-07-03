6c01cfb0c31ba65344b9b36cad760db287f2ba3b
Shahules786 <Shahules786@gmail.com>
2024-07-03 13:03:11 +0530

    add simple test generator

---
+++ src/experimental/ragas_experimental/testset/generators/__init__.py
@@ -0,0 +1,8 @@
+from ragas_experimental.testset.generators.base import QADistribution, TestGenerator
+from ragas_experimental.testset.generators.simple import SimpleTestGenerator
+
+__all__ = [
+    "TestGenerator",
+    "SimpleTestGenerator",
+    "QADistribution",
+]
