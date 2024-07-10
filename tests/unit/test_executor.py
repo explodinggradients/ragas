55d46004cac162ac562ea0c86ec24ce14dfa4b79
jjmachan <jamesjithin97@gmail.com>
2024-07-10 23:46:06 +0530

    added tests

--- tests/unit/test_executor.py
+++ tests/unit/test_executor.py
@@ -1,7 +1,10 @@
+from asyncio import sleep
+
+
 def test_order_of_execution():
     from ragas.executor import Executor
 
-    async def echo_order(index):
+    async def echo_order(index: int):
         return index
 
     # Arrange
@@ -14,3 +17,44 @@ def test_order_of_execution():
     results = executor.results()
     # Assert
     assert results == list(range(1, 11))
+
+
+def test_executor_in_script():
+    from ragas.executor import Executor
+
+    async def echo_order(index: int):
+        await sleep(index)
+        return index
+
+    # Arrange
+    executor = Executor()
+
+    # Act
+    # add 10 jobs to the executor
+    for i in range(1, 4):
+        executor.submit(echo_order, i, name=f"echo_order_{i}")
+    results = executor.results()
+    # Assert
+    assert results == list(range(1, 4))
+
+
+def test_executor_with_running_loop():
+    import asyncio
+    from ragas.executor import Executor
+
+    loop = asyncio.new_event_loop()
+
+    async def echo_order(index: int):
+        await sleep(1)
+        return index
+
+    # Arrange
+    executor = Executor()
+
+    # Act
+    # add 10 jobs to the executor
+    for i in range(1, 4):
+        executor.submit(echo_order, i, name=f"echo_order_{i}")
+    results = executor.results()
+    # Assert
+    assert results == list(range(1, 4))
