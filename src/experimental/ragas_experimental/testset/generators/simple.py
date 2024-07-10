931a773cdd4100ec280ead59e2439cf924a52b7c
jjmachan <jamesjithin97@gmail.com>
2024-07-10 12:14:08 +0530

    change to correct TestDataset

--- src/experimental/ragas_experimental/testset/generators/simple.py
+++ src/experimental/ragas_experimental/testset/generators/simple.py
@@ -29,12 +29,12 @@ from ragas_experimental.testset.relationships import (
 )
 from ragas_experimental.testset.splitters import HeadlineSplitter
 from ragas_experimental.testset.utils import rng
+from ragas_experimental.testset.generators.base import TestDataset
 
 from ragas.embeddings import embedding_factory
 from ragas.executor import Executor
 from ragas.llms.base import llm_factory
 from ragas.utils import check_if_sum_is_close
-from ragas.testset.generator import TestDataset
 
 abstract_qa = AbstractQA(distribution=DEFAULT_DISTRIBUTION)
 comparative_qa = ComparativeAbstractQA(distribution=DEFAULT_DISTRIBUTION)
