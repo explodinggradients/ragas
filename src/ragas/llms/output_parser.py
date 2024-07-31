5f922829b8de49b20035a5a3bac5bfd85d1b0bdd
Emre Demir <emredemireng@gmail.com>
2024-07-31 20:55:43 +0200

    update on json format prompt.

--- src/ragas/llms/output_parser.py
+++ src/ragas/llms/output_parser.py
@@ -30,6 +30,7 @@ JSON_FORMAT_INSTRUCTIONS = """The output should be a well-formatted JSON instanc
 As an example, for the schema {{"properties": {{"foo": {{"title": "Foo", "description": "a list of strings", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}
 the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of the schema. The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted.
 
+Never return the answer as a raw string, always return it as a JSON object with the following schema.
 Here is the output JSON schema:
 ```
 {schema}
