79464809f2960d6ce9567a3da4010a5ccf68ba2a
ikka <shahules786@gmail.com>
2024-08-13 12:16:50 +0530

    metrics: add faithfulness with HHEM model (#1191)
    Ragas integration for
    https://vectara.com/blog/hhem-2-1-a-better-hallucination-detection-model/

--- src/ragas/metrics/__init__.py
+++ src/ragas/metrics/__init__.py
@@ -12,7 +12,7 @@ from ragas.metrics._context_precision import (
     context_utilization,
 )
 from ragas.metrics._context_recall import ContextRecall, context_recall
-from ragas.metrics._faithfulness import Faithfulness, faithfulness
+from ragas.metrics._faithfulness import Faithfulness, FaithulnesswithHHEM, faithfulness
 from ragas.metrics._summarization import SummarizationScore, summarization_score
 from ragas.metrics.critique import AspectCritique
 
@@ -21,6 +21,7 @@ __all__ = [
     "answer_correctness",
     "Faithfulness",
     "faithfulness",
+    "FaithulnesswithHHEM",
     "AnswerSimilarity",
     "answer_similarity",
     "ContextPrecision",
