0ac33851a1543887195d33bedeed7b6945158e98
Shahules786 <Shahules786@gmail.com>
2024-08-28 11:25:35 +0530

    add messages

--- src/ragas/dataset_schema.py
+++ src/ragas/dataset_schema.py
@@ -1,7 +1,40 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Literal, Optional, Union
 
 from datasets import Dataset
-from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.pydantic_v1 import BaseModel, validator
+
+
+class Message(BaseModel):
+    content: str
+
+
+class ToolCall(BaseModel):
+    name: str
+    args: Dict[str, Union[str, int, float]]
+
+
+class HumanMessage(Message):
+    type: Literal["human"] = "human"
+
+
+class ToolMessage(Message):
+    type: Literal["tool"] = "tool"
+
+
+class AIMessage(Message):
+    type: Literal["ai"] = "ai"
+    tool_calls: Optional[List[ToolCall]] = None
+
+    def dict(self, **kwargs):
+        content = (
+            self.content
+            if self.tool_calls is None
+            else {
+                "text": self.content,
+                "tool_calls": [tc.dict() for tc in self.tool_calls],
+            }
+        )
+        return {"content": content, "type": self.type}
 
 
 class EvaluationSample(BaseModel):
@@ -9,8 +42,8 @@ class EvaluationSample(BaseModel):
     retrieved_contexts: Optional[List[str]] = None
     ground_truth_contexts: Optional[List[str]] = None
     response: Optional[str] = None
-    ground_truth: Optional[str] = None
-    rubric: Optional[Dict] = None
+    reference: Optional[str] = None
+    rubric: Optional[Dict[str, str]] = None
 
     def to_dict(self):
         row = {
@@ -18,11 +51,26 @@ class EvaluationSample(BaseModel):
             "retrieved_contexts": self.retrieved_contexts,
             "ground_truth_contexts": self.ground_truth_contexts,
             "response": self.response,
-            "ground_truth": self.ground_truth,
+            "reference": self.reference,
             "rubric": self.rubric,
         }
         row = {k: v for k, v in row.items() if v is not None}
         return row
+
+
+class WorkflowEvaluationSample(BaseModel):
+    user_input: List[Union[HumanMessage, AIMessage, ToolMessage]]
+
+    @validator("user_input")
+    def validate_messages(cls, v):
+        if not (isinstance(m, (HumanMessage, AIMessage, ToolMessage)) for m in v):
+            raise ValueError(
+                f"All inputs must be instances of HumanMessage, AIMessage, or ToolMessage. Got: {v}"
+            )
+        return v
+
+    def to_messages(self):
+        return [m.dict() for m in self.user_input]
 
 
 class EvaluationDataset(BaseModel):
