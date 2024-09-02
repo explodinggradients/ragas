fa864a678effff4300323065d019cbd04e70de51
Miaoran Li <50842750+Miaoranmmm@users.noreply.github.com>
2024-09-02 12:56:43 -0500

    load HHEM on specified device (#1235)
    Allow users to specify the device to load HHEM and add `_create_batch`
    to avoid OOM
    
    ---------
    
    Co-authored-by: Miaoran <miaoran@vectara.com>
    Co-authored-by: jjmachan <jamesjithin97@gmail.com>

--- src/ragas/metrics/_faithfulness.py
+++ src/ragas/metrics/_faithfulness.py
@@ -17,11 +17,9 @@ if t.TYPE_CHECKING:
 
     from ragas.llms.prompt import PromptValue
 
-from typing import Any, Protocol
 
-
-class HasSegmentMethod(Protocol):
-    def segment(self, text) -> Any:
+class HasSegmentMethod(t.Protocol):
+    def segment(self, text) -> t.Any:
         ...
 
 
@@ -316,6 +314,8 @@ class Faithfulness(MetricWithLLM):
 @dataclass
 class FaithulnesswithHHEM(Faithfulness):
     name: str = "faithfulness_with_hhem"  # type: ignore
+    device: str = "cpu"
+    batch_size: int = 10
 
     def __post_init__(self):
         try:
@@ -327,6 +327,7 @@ class FaithulnesswithHHEM(Faithfulness):
         self.nli_classifier = AutoModelForSequenceClassification.from_pretrained(
             "vectara/hallucination_evaluation_model", trust_remote_code=True
         )
+        self.nli_classifier.to(self.device)
         super().__post_init__()
 
     def _create_pairs(
@@ -338,6 +339,13 @@ class FaithulnesswithHHEM(Faithfulness):
         premise = "\n".join(row["contexts"])
         pairs = [(premise, statement) for statement in statements]
         return pairs
+
+    def _create_batch(
+        self, pairs: t.List[t.Tuple[str, str]]
+    ) -> t.Generator[t.List[t.Tuple[str, str]], None, None]:
+        length_of_pairs = len(pairs)
+        for ndx in range(0, length_of_pairs, self.batch_size):
+            yield pairs[ndx : min(ndx + self.batch_size, length_of_pairs)]
 
     async def _ascore(self: t.Self, row: t.Dict, callbacks: Callbacks) -> float:
         """
@@ -362,9 +370,14 @@ class FaithulnesswithHHEM(Faithfulness):
 
         assert isinstance(statements, t.List), "statements must be a list"
 
+        scores = []
         pairs = self._create_pairs(row, statements)
-        scores = self.nli_classifier.predict(pairs).detach().numpy().round()
-        return scores.sum() / len(scores)
+        for input_pairs in self._create_batch(pairs):  # to avoid OOM
+            batch_scores = (
+                self.nli_classifier.predict(input_pairs).cpu().detach().round()
+            )
+            scores += batch_scores
+        return sum(scores) / len(scores)
 
 
 faithfulness = Faithfulness()
