78c71efd6ff69e4fb4577b38b29bca50b3edc823
Shahules786 <Shahules786@gmail.com>
2024-09-03 12:41:00 +0530

    remove old validation logic

--- src/ragas/validation.py
+++ src/ragas/validation.py
@@ -5,8 +5,7 @@ import logging
 from datasets import Dataset, Sequence
 
 from ragas.dataset_schema import EvaluationDataset
-from ragas.metrics._context_precision import ContextPrecision
-from ragas.metrics.base import EvaluationMode, Metric
+from ragas.metrics.base import Metric
 
 logger = logging.getLogger(__name__)
 
@@ -38,68 +37,6 @@ def handle_deprecated_ground_truths(ds: Dataset) -> Dataset:
                 + "a",  # adding random to fingerprint to avoid caching
             )
     return ds
-
-
-def validate_column_dtypes(ds: Dataset):
-    for column_names in ["question", "answer", "ground_truth"]:
-        if column_names in ds.features:
-            if ds.features[column_names].dtype != "string":
-                raise ValueError(
-                    f'Dataset feature "{column_names}" should be of type string'
-                )
-
-    for column_names in ["contexts"]:
-        if column_names in ds.features:
-            if not (
-                isinstance(ds.features[column_names], Sequence)
-                and ds.features[column_names].feature.dtype == "string"
-            ):
-                raise ValueError(
-                    f'Dataset feature "{column_names}" should be of type'
-                    f" Sequence[string], got {type(ds.features[column_names])}"
-                )
-
-
-EVALMODE_TO_COLUMNS = {
-    EvaluationMode.qac: ["question", "answer", "contexts"],
-    EvaluationMode.qa: ["question", "answer"],
-    EvaluationMode.qc: ["question", "contexts"],
-    EvaluationMode.gc: ["ground_truth", "contexts"],
-    EvaluationMode.ga: ["ground_truth", "answer"],
-    EvaluationMode.qga: ["question", "ground_truth", "answer"],
-    EvaluationMode.qcg: ["question", "contexts", "ground_truth"],
-    EvaluationMode.ca: ["contexts", "summary"],
-}
-
-
-def validate_evaluation_modes(
-    ds: Dataset, metrics: list[Metric], evalmode_to_columns: dict = EVALMODE_TO_COLUMNS
-):
-    """
-    validates the dataset and returns the evaluation type
-
-    possible evaluation types
-    1. (q,a,c)
-    2. (q,a)
-    3. (q,c)
-    4. (g,a)
-    """
-    for m in metrics:
-        required_columns = set(evalmode_to_columns[m.evaluation_mode])
-        available_columns = set(ds.features.keys())
-        if not required_columns.issubset(available_columns):
-            extra_msg = ""
-            if (
-                isinstance(m, ContextPrecision)
-                and "ground_truth" not in available_columns
-            ):
-                extra_msg = "Looks like you're trying to use 'context_precision' without ground_truth. Please use consider using  `context_utilization' instead."
-
-            raise ValueError(
-                f"The metric [{m.name}] that that is used requires the following "
-                f"additional columns {list(required_columns - available_columns)} "
-                f"to be present in the dataset. {extra_msg}"
-            )
 
 
 def validate_required_columns(ds: EvaluationDataset, metrics: list[Metric]):
