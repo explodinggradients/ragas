9152bbefc1b4be9864322ebca2bb6a4b80b7b24a
jjmachan <jamesjithin97@gmail.com>
2024-09-06 12:28:03 +0530

    changed prompt object location and function

--- src/experimental/ragas_experimental/prompt.py
+++ src/experimental/ragas_experimental/prompt.py
@@ -1,30 +1,29 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
-import json
 import typing as t
+from abc import ABC, abstractmethod
+
+import pydantic
+
+# Check Pydantic version
+from pydantic import BaseModel
 
 from ragas.llms.output_parser import RagasoutputParser
 from ragas.llms.prompt import PromptValue
 
-# Check Pydantic version
-from pydantic import BaseModel
-import pydantic
-
 if t.TYPE_CHECKING:
-    from ragas.llms.base import BaseRagasLLM
     from langchain_core.callbacks import Callbacks
+
+    from ragas.llms.base import BaseRagasLLM
 
 PYDANTIC_V2 = pydantic.VERSION.startswith("2.")
 
 
 class BasePrompt(ABC):
-    def __init__(self, llm):
-        self.llm: BaseRagasLLM = llm
-
     @abstractmethod
-    async def generate(self, data: t.Any) -> t.Any:
+    async def generate(
+        self, llm: BaseRagasLLM, data: t.Any, callbacks: Callbacks
+    ) -> t.Any:
         pass
 
 
@@ -63,6 +62,7 @@ def model_to_json_schema(model: t.Type[BaseModel]) -> dict:
     else:
         return model.schema_json()
 
+
 InputModel = t.TypeVar("InputModel", bound=BaseModel)
 OutputModel = t.TypeVar("OutputModel", bound=BaseModel)
 
@@ -96,9 +96,11 @@ class PydanticPrompt(BasePrompt, t.Generic[InputModel, OutputModel]):
                 example_strings.append(
                     self.instruction
                     + "\n"
-                    + "input: " + to_json(input_data, indent=4)
+                    + "input: "
+                    + to_json(input_data, indent=4)
                     + "\n"
-                    + "output: " + to_json(output_data, indent=4)
+                    + "output: "
+                    + to_json(output_data, indent=4)
                 )
 
             return (
@@ -118,24 +120,27 @@ class PydanticPrompt(BasePrompt, t.Generic[InputModel, OutputModel]):
             + "\n"
             + self.generate_examples()
             + "\nNow perform the above instruction with the following input\n"
-            + "input: " + to_json(data, indent=4)
+            + "input: "
+            + to_json(data, indent=4)
             + "\n"
             + "output: "
         )
 
-    async def generate(self, data: InputModel, callbacks: Callbacks) -> OutputModel:
+    async def generate(
+        self, llm: BaseRagasLLM, data: InputModel, callbacks: Callbacks
+    ) -> OutputModel:
         prompt_value = PromptValue(prompt_str=self.to_string(data))
-        resp = await self.llm.generate(prompt_value, callbacks=callbacks)
+        resp = await llm.generate(prompt_value, callbacks=callbacks)
         resp_text = resp.generations[0][0].text
         parser = RagasoutputParser(pydantic_object=self.output_model)
-        answer = await parser.aparse(resp_text, prompt_value, self.llm, max_retries=3)
+        answer = await parser.aparse(resp_text, prompt_value, llm, max_retries=3)
 
         # TODO: make sure RagasOutputPraser returns the same type as OutputModel
         return answer  # type: ignore
 
 
 class StringPrompt(BasePrompt):
-    async def generate(self, data: str) -> str:
+    async def generate(self, llm: BaseRagasLLM, data: str, callbacks: Callbacks) -> str:
         prompt_value = PromptValue(prompt_str=data)
-        llm_result = await self.llm.agenerate_text(prompt_value)
+        llm_result = await llm.agenerate_text(prompt_value, callbacks=callbacks)
         return llm_result.generations[0][0].text
