625c7b8e21ea5beb9d9733d13da25e753bc0492c
Shahules786 <Shahules786@gmail.com>
2024-09-09 18:47:54 +0530

    move rogue score

--- src/ragas/metrics/bleu_score.py
+++ src/ragas/metrics/bleu_score.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from langchain_core.callbacks import Callbacks
 from nltk.tokenize import word_tokenize
 from nltk.translate.bleu_score import corpus_bleu
+from rouge_score import rouge_scorer
 
 from ragas.dataset_schema import SingleTurnSample
 from ragas.metrics._faithfulness import HasSegmentMethod
@@ -38,6 +39,26 @@ class BleuScore(SingleTurnMetric):
 
     async def _ascore(self, row: t.Dict, callbacks: Callbacks) -> float:
         return await self._single_turn_ascore(SingleTurnSample(**row), callbacks)
+
+
+@dataclass
+class RougeScore(SingleTurnMetric):
+    name: str = "rouge_score"  # type: ignore
+    _required_columns: t.Tuple[str, ...] = ("reference", "response")
+    rogue_type: t.Literal["rouge1", "rougeL"] = "rougeL"
+    measure_type: t.Literal["fmeasure", "precision", "recall"] = "fmeasure"
+
+    def init(self, run_config: RunConfig):
+        pass
+
+    async def _single_turn_ascore(
+        self, sample: SingleTurnSample, callbacks: Callbacks
+    ) -> float:
+        assert isinstance(sample.reference, str), "Sample reference must be a string"
+        assert isinstance(sample.response, str), "Sample response must be a string"
+        scorer = rouge_scorer.RougeScorer([self.rogue_type], use_stemmer=True)
+        scores = scorer.score(sample.reference, sample.response)
+        return getattr(scores[self.rogue_type], self.measure_type)
 
 
 bleu_score = BleuScore()
