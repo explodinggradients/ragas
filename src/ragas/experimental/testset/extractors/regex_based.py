b6ca9fdb89a3447115e96558f50489237b466aed
jjmachan <jamesjithin97@gmail.com>
2024-09-10 18:11:26 +0530

    added regex based extractors

--- src/ragas/experimental/testset/extractors/regex_based.py
+++ src/ragas/experimental/testset/extractors/regex_based.py
@@ -14,6 +14,10 @@ class RegexBasedExtractor(BaseExtractor):
 
     async def _extract(self, node: Node) -> t.Tuple[str, t.Any]:
         text = node.get_property("page_content")
+        if not isinstance(text, str):
+            raise ValueError(
+                f"node.property('page_content') must be a string, found '{type(text)}'"
+            )
 
         matches = (
             re.finditer(self.pattern, text, re.MULTILINE)
@@ -26,3 +30,23 @@ class RegexBasedExtractor(BaseExtractor):
             for key, value in m_dict.items():
                 result[key].append(value)
         return result
+
+
+# This regex pattern matches URLs, including those starting with "http://", "https://", or "www."
+links_extractor_pattern = r"(?i)\b(?:https?://|www\.)\S+\b"
+links_extractor = RegexBasedExtractor(
+    pattern=links_extractor_pattern, is_multiline=True, property_name="links"
+)
+
+# This regex pattern matches emails, which typically follow the format "username@domain.extension".
+emails_extractor_pattern = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
+emails_extractor = RegexBasedExtractor(
+    pattern=emails_extractor_pattern, is_multiline=False, property_name="emails"
+)
+
+# This regex pattern matches Markdown headings, which start with a number sign (#) followed by a space,
+# and the rest of the line is the heading text.
+markdown_headings_pattern = r"^(#{1,6})\s+(.*)"
+markdown_headings_extractor = RegexBasedExtractor(
+    pattern=markdown_headings_pattern, is_multiline=True, property_name="headings"
+)
