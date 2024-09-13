a45f4c0c42ded7c55b937e74e1bd298e4278baef
jjmachan <jamesjithin97@gmail.com>
2024-09-13 06:53:39 +0530

    fix merge mistake

--- src/ragas/metrics/__init__.py
+++ src/ragas/metrics/__init__.py
@@ -29,12 +29,6 @@ from ragas.metrics._noise_sensitivity import (
     noise_sensitivity_relevant,
 )
 from ragas.metrics._summarization import SummarizationScore, summarization_score
-from ragas.metrics.domain_specific_rubrics import (
-    RubricsScoreWithoutReference,
-    RubricsScoreWithReference,
-    rubrics_score_with_reference,
-    rubrics_score_without_reference,
-)
 
 __all__ = [
     "AnswerCorrectness",
