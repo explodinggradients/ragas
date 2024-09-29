6dbcf6da9f452342577b50e336e540697ca61331
jjmachan <jamesjithin97@gmail.com>
2024-09-29 18:30:40 +0530

    putting it together

--- src/ragas/experimental/testset/simulators/__init__.py
+++ src/ragas/experimental/testset/simulators/__init__.py
@@ -2,14 +2,19 @@ import typing as t
 
 from ragas.llms import BaseRagasLLM
 
-from .abstract_qa import AbstractQuestionSimulator
+from .abstract_qa import AbstractQuestionSimulator, ComparativeAbstractQuestionSimulator
 from .base import BaseSimulator
+from .specific_qa import SpecificQASimulator
 
 QuestionTypes = t.List[t.Tuple[BaseSimulator, float]]
 
 
 def default_scenarios(llm: BaseRagasLLM) -> QuestionTypes:
-    return [(AbstractQuestionSimulator(llm), 1.0)]
+    return [
+        (AbstractQuestionSimulator(llm), 0.25),
+        (ComparativeAbstractQuestionSimulator(llm), 0.25),
+        (SpecificQASimulator(llm), 0.5),
+    ]
 
 
 __all__ = ["AbstractQuestionSimulator", "default_scenarios"]
