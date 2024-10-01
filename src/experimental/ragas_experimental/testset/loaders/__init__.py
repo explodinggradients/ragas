4a9601f346dd0e3fe33e5cbf03dffb0f42dc0758
jjmachan <jamesjithin97@gmail.com>
2024-10-01 15:45:38 +0530

    rm experimetal

--- src/experimental/ragas_experimental/testset/loaders/__init__.py
+++
@@ -1,3 +0,0 @@
-from ragas_experimental.testset.loaders.ragas_loader import RAGASLoader
-
-__all__ = ["RAGASLoader"]
--- src/experimental/ragas_experimental/testset/splitters/__init__.py
+++
@@ -1,3 +0,0 @@
-from ragas_experimental.testset.splitters.section_splitter import HeadlineSplitter
-
-__all__ = ["HeadlineSplitter"]
