543cc327554a5f7fd771f6bc0be79ebeb5bcf5b7
jjmachan <jamesjithin97@gmail.com>
2024-10-01 20:17:58 +0530

    don't init the python object

--- src/ragas/testset/transforms/extractors/embeddings.py
+++ src/ragas/testset/transforms/extractors/embeddings.py
@@ -1,5 +1,5 @@
 import typing as t
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from ragas.embeddings import BaseRagasEmbeddings, embedding_factory
 from ragas.testset.graph import Node
@@ -8,10 +8,9 @@ from ragas.testset.transforms.base import Extractor
 
 @dataclass
 class EmbeddingExtractor(Extractor):
-    model: str = "text-embedding-3-small"
     property_name: str = "embedding"
     embed_property_name: str = "page_content"
-    embedding_model: BaseRagasEmbeddings = embedding_factory(model=model)
+    embedding_model: BaseRagasEmbeddings = field(default_factory=embedding_factory)
 
     async def extract(self, node: Node) -> t.Tuple[str, t.Any]:
         text = node.get_property(self.embed_property_name)
@@ -21,6 +20,3 @@ class EmbeddingExtractor(Extractor):
             )
         embedding = self.embedding_model.embed_query(text)
         return self.property_name, embedding
-
-
-embedding_extractor = EmbeddingExtractor()
