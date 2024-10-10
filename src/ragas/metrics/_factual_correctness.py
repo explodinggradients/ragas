b2121440143817fa3400183d361e11e9f26f433a
jjmachan <jamesjithin97@gmail.com>
2024-10-10 12:27:29 +0530

    refactor: moved answer_correctness to new prompt object

--- src/ragas/metrics/_factual_correctness.py
+++ src/ragas/metrics/_factual_correctness.py
@@ -9,7 +9,7 @@ import numpy as np
 from numpy.typing import NDArray
 from pydantic import BaseModel, Field
 
-from ragas.experimental.metrics._faithfulness import (
+from ragas.metrics._faithfulness import (
     NLIStatementInput,
     NLIStatementPrompt,
 )
