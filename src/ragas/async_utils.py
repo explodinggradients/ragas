aa30b89e85328cdcc73a3d64896754c2e9b9fdac
ahgraber <ahgraber@ninerealmlabs.com>
2024-10-27 15:45:46 -0400

    feat: add batched execution to run_async_tasks

--- src/ragas/async_utils.py
+++ src/ragas/async_utils.py
@@ -1,49 +1,71 @@
 """Async utils."""
 
 import asyncio
-from typing import Any, Coroutine, List
+import typing as t
+
+from tqdm.asyncio import tqdm as atqdm
+
+from ragas.executor import is_event_loop_running
+from ragas.utils import batched
 
 
 def run_async_tasks(
-    tasks: List[Coroutine],
-    show_progress: bool = False,
+    tasks: t.List[t.Coroutine],
+    batch_size: t.Optional[int] = None,
+    show_progress: bool = True,
     progress_bar_desc: str = "Running async tasks",
-) -> List[Any]:
+) -> t.List[t.Any]:
     """Run a list of async tasks."""
-    tasks_to_execute: List[Any] = tasks
 
-    # if running in notebook, use nest_asyncio to hijack the event loop
-    try:
-        loop = asyncio.get_running_loop()
+    async def _process_batch(
+        batch: t.Iterable[t.Coroutine],
+        progress_bar_desc: str = "Batch progress",
+    ) -> t.List[t.Tuple[int, t.Coroutine]]:
+        """Process batch of jobs with optional progress tracking."""
+
+        return await atqdm.gather(
+            *list(batch),
+            desc=progress_bar_desc,
+            disable=not show_progress,
+        )
+
+    async def _process_jobs() -> t.List[t.Any]:
+        """Execute jobs with optional progress tracking."""
+
+        if batch_size:
+            results = []
+            batches = batched(tasks, batch_size)  # generator
+            n_batches = (len(tasks) + batch_size - 1) // batch_size
+
+            with atqdm(
+                total=n_batches,
+                desc=progress_bar_desc,
+                disable=not show_progress,
+            ) as pbar:
+                for i, batch in enumerate(batches, 1):
+                    batch_results = await _process_batch(
+                        batch,
+                        progress_bar_desc=f"Batch {i}/{n_batches}",
+                    )
+                    results.extend(batch_results)
+                    pbar.update(1)
+
+        else:
+            results = await _process_batch(tasks, progress_bar_desc=progress_bar_desc)
+
+        return results
+
+    if is_event_loop_running():
+        # an event loop is running so call nested_asyncio to fix this
         try:
             import nest_asyncio
         except ImportError:
-            raise RuntimeError(
-                "nest_asyncio is required to run async tasks in jupyter. Please install it via `pip install nest_asyncio`."  # noqa
+            raise ImportError(
+                "It seems like your running this in a jupyter-like environment. "
+                "Please install nest_asyncio with `pip install nest_asyncio` to make it work."
             )
-        else:
+        finally:
             nest_asyncio.apply()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
 
-    # gather tasks to run
-    if show_progress:
-        from tqdm.asyncio import tqdm
-
-        async def _gather() -> List[Any]:
-            "gather tasks and show progress bar"
-            return await tqdm.gather(*tasks_to_execute, desc=progress_bar_desc)
-
-    else:  # don't show_progress
-
-        async def _gather() -> List[Any]:
-            return await asyncio.gather(*tasks_to_execute)
-
-    try:
-        outputs: List[Any] = loop.run_until_complete(_gather())
-    except Exception as e:
-        # run the operation w/o tqdm on hitting a fatal
-        # may occur in some environments where tqdm.asyncio
-        # is not supported
-        raise RuntimeError("Fatal error occurred while running async tasks.", e) from e
-    return outputs
+    results = asyncio.run(_process_jobs())
+    return results
