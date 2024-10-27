aea282b95c1daf6faa76e2a72e79c0e4c40e0626
ahgraber <ahgraber@ninerealmlabs.com>
2024-10-27 15:46:09 -0400

    feat: tests

--- tests/unit/test_executor.py
+++ tests/unit/test_executor.py
@@ -2,69 +2,7 @@ import asyncio
 
 import pytest
 
-
-@pytest.mark.asyncio
-async def test_order_of_execution():
-    from ragas.executor import Executor
-
-    async def echo_order(index: int):
-        return index
-
-    # Arrange
-    executor = Executor()
-
-    # Act
-    # add 10 jobs to the executor
-    for i in range(1, 11):
-        executor.submit(echo_order, i, name=f"echo_order_{i}")
-    results = executor.results()
-    # Assert
-    assert results == list(range(1, 11))
-
-
-@pytest.mark.asyncio
-async def test_executor_in_script():
-    from ragas.executor import Executor
-
-    async def echo_order(index: int):
-        await asyncio.sleep(0.1)
-        return index
-
-    # Arrange
-    executor = Executor()
-
-    # Act
-    # add 10 jobs to the executor
-    for i in range(1, 4):
-        executor.submit(echo_order, i, name=f"echo_order_{i}")
-    results = executor.results()
-    # Assert
-    assert results == list(range(1, 4))
-
-
-@pytest.mark.asyncio
-async def test_executor_with_running_loop():
-    import asyncio
-
-    from ragas.executor import Executor
-
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(asyncio.sleep(0.1))
-
-    async def echo_order(index: int):
-        await asyncio.sleep(0.1)
-        return index
-
-    # Arrange
-    executor = Executor()
-
-    # Act
-    # add 10 jobs to the executor
-    for i in range(1, 4):
-        executor.submit(echo_order, i, name=f"echo_order_{i}")
-    results = executor.results()
-    # Assert
-    assert results == list(range(1, 4))
+from ragas.executor import Executor
 
 
 def test_is_event_loop_running_in_script():
@@ -77,7 +15,7 @@ def test_as_completed_in_script():
     from ragas.executor import as_completed
 
     async def echo_order(index: int):
-        await asyncio.sleep(index)
+        await asyncio.sleep(0.1)
         return index
 
     async def _run():
@@ -90,3 +28,63 @@ def test_as_completed_in_script():
     results = asyncio.run(_run())
 
     assert results == [1, 2, 3]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("batch_size", [None, 3, 20])
+async def test_order_of_execution(batch_size):
+    async def echo_order(index: int):
+        await asyncio.sleep(1 / index)
+        return index
+
+    # Arrange
+    executor = Executor(batch_size=batch_size)
+    # add 10 jobs to the executor
+    for i in range(1, 11):
+        executor.submit(echo_order, i, name=f"echo_order_{i}")
+
+    # Act
+    results = executor.results()
+    # Assert
+    assert results == list(range(1, 11))
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("batch_size", [None, 3, 20])
+async def test_executor_in_script(batch_size):
+    async def echo_order(index: int):
+        await asyncio.sleep(1 / index)
+        return index
+
+    # Arrange
+    executor = Executor(batch_size=batch_size)
+    # add 10 jobs to the executor
+    for i in range(1, 4):
+        executor.submit(echo_order, i, name=f"echo_order_{i}")
+
+    # Act
+    results = executor.results()
+    # Assert
+    assert results == list(range(1, 4))
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("batch_size", [None, 3, 20])
+async def test_executor_with_running_loop(batch_size):
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(asyncio.sleep(0.1))
+
+    async def echo_order(index: int):
+        await asyncio.sleep(1 / index)
+        return index
+
+    # Arrange
+    executor = Executor(batch_size=batch_size)
+    for i in range(1, 4):
+        executor.submit(echo_order, i, name=f"echo_order_{i}")
+
+    # Act
+    # add 10 jobs to the executor
+    results = executor.results()
+    # Assert
+    assert results == list(range(1, 4))
