c58ff36772bc83871db56521192c75e81c177ee9
jjmachan <jamesjithin97@gmail.com>
2024-11-01 12:47:20 +0530

    fix: type annotation for metric

--- src/ragas/evaluation.py
+++ src/ragas/evaluation.py
@@ -55,7 +55,7 @@ RAGAS_EVALUATION_CHAIN_NAME = "ragas evaluation"
 @track_was_completed
 def evaluate(
     dataset: t.Union[Dataset, EvaluationDataset],
-    metrics: list[Metric] | None = None,
+    metrics: t.Optional[t.Sequence[Metric]] = None,
     llm: t.Optional[BaseRagasLLM | LangchainLLM] = None,
     embeddings: t.Optional[BaseRagasEmbeddings | LangchainEmbeddings] = None,
     callbacks: Callbacks = None,
