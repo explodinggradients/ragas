6e2c387a30a19205cbce6fd25bc42df600141c69
Shahules786 <Shahules786@gmail.com>
2024-11-04 16:37:35 +0530

    add persona to init

--- src/ragas/testset/__init__.py
+++ src/ragas/testset/__init__.py
@@ -1,4 +1,12 @@
+from ragas.testset.persona import Persona, PersonaGenerator, PersonasList
 from ragas.testset.synthesizers.generate import TestsetGenerator
 from ragas.testset.synthesizers.testset_schema import Testset, TestsetSample
 
-__all__ = ["TestsetGenerator", "Testset", "TestsetSample"]
+__all__ = [
+    "TestsetGenerator",
+    "Testset",
+    "TestsetSample",
+    "PersonaGenerator",
+    "Persona",
+    "PersonasList",
+]
