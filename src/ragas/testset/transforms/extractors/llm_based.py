1d3f15a6606e6216d61978aa511ec033b9a1f411
Shahules786 <Shahules786@gmail.com>
2024-11-05 15:32:00 +0530

    redo headline extraction

--- src/ragas/testset/transforms/extractors/llm_based.py
+++ src/ragas/testset/transforms/extractors/llm_based.py
@@ -66,44 +66,38 @@ class TitleExtractorPrompt(PydanticPrompt[StringIO, StringIO]):
 
 
 class Headlines(BaseModel):
-    headlines: t.Dict[str, t.List[str]]
+    headlines: t.List[str]
 
 
 class HeadlinesExtractorPrompt(PydanticPrompt[StringIO, Headlines]):
-    instruction: str = "Extract the headlines from the given text."
+    instruction: str = (
+        "Extract only the level 2 headings from the given text if they are present."
+    )
     input_model: t.Type[StringIO] = StringIO
     output_model: t.Type[Headlines] = Headlines
     examples: t.List[t.Tuple[StringIO, Headlines]] = [
         (
             StringIO(
                 text="""\
-Some Title
-1. Introduction and Related Work
+        Section 1: Introduction
+        Introduction to the topic...
 
-1.1 Conditional Computation
-Exploiting scale in both training data and model size has been central to the success of deep learning...
-1.2 Our Approach: The Sparsely-Gated Mixture-of-Experts Layer
-Our approach to conditional computation is to introduce a new type of general purpose neural network component...
-1.3 Related Work on Mixtures of Experts
-Since its introduction more than two decades ago (Jacobs et al., 1991; Jordan & Jacobs, 1994), the mixture-of-experts approach..
+        1. Main Concepts
+        1.1 Key Definitions
+        Explanation of core definitions...
 
-2. The Sparsely-Gated Mixture-of-Experts Layer
-2.1 Architecture
-The sparsely-gated mixture-of-experts layer is a feedforward neural network layer that consists of a number of expert networks and a single gating network...
-""",
+        2. Advanced Topics
+        2.1 Specialized Techniques
+        Detail on various advanced techniques...
+
+        2.2 Emerging Trends
+        Description of current and emerging trends...
+
+        3. Summary and Conclusion
+        Final remarks and summary.
+        """,
             ),
-            Headlines(
-                headlines={
-                    "1. Introduction and Related Work": [
-                        "1.1 Conditional Computation",
-                        "1.2 Our Approach: The Sparsely-Gated Mixture-of-Experts Layer",
-                        "1.3 Related Work on Mixtures of Experts",
-                    ],
-                    "2. The Sparsely-Gated Mixture-of-Experts Layer": [
-                        "2.1 Architecture"
-                    ],
-                },
-            ),
+            Headlines(headlines=["2.1 Specialized Techniques", "2.2 Emerging Trends"]),
         ),
     ]
 
