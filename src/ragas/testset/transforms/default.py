134153626155ff92a2494f51efaa2a3e2bc88a4a
Shahules786 <Shahules786@gmail.com>
2024-11-14 13:40:52 +0530

    tune default transforms

--- src/ragas/testset/transforms/default.py
+++ src/ragas/testset/transforms/default.py
@@ -27,6 +27,7 @@ if t.TYPE_CHECKING:
 
 from langchain_core.documents import Document as LCDocument
 
+
 def default_transforms(
     documents: t.List[LCDocument],
     llm: BaseRagasLLM,
@@ -48,11 +49,13 @@ def default_transforms(
         A list of transformation steps to be applied to the knowledge graph.
 
     """
-    
+
     def count_doc_length_bins(documents, bin_ranges):
-        data = [num_tokens_from_string(doc.properties["page_content"]) for doc in documents]
+        data = [
+            num_tokens_from_string(doc.page_content) for doc in documents
+        ]
         bins = {f"{start}-{end}": 0 for start, end in bin_ranges}
-        
+
         for num in data:
             for start, end in bin_ranges:
                 if start <= num <= end:
@@ -60,37 +63,41 @@ def default_transforms(
                     break  # Move to the next number once it’s placed in a bin
 
         return bins
-    
+
     def filter_doc_with_num_tokens(node, min_num_tokens=500):
         return (
             node.type == NodeType.DOCUMENT
             and num_tokens_from_string(node.properties["page_content"]) > min_num_tokens
         )
-        
+
     def filter_docs(node):
         return node.type == NodeType.DOCUMENT
-    
+
     def filter_chunks(node):
         return node.type == NodeType.CHUNK
-    
-    bin_ranges = [(0, 500), (501, 100000)]
-    result = count_doc_length_bins(data, bin_ranges)
-    result = {k: v/len(documents) for k, v in result.items()}
-    
+
+    bin_ranges = [(0,100),(101, 500), (501, 100000)]
+    result = count_doc_length_bins(documents, bin_ranges)
+    result = {k: v / len(documents) for k, v in result.items()}
+
     transforms = []
-    
-    if result["501-100000"] > 0.1:
-        headline_extractor = HeadlinesExtractor(llm=llm, filter_nodes=lambda node: filter_doc_with_num_tokens(node))
+
+    if result["501-100000"] >= 0.5:
+        headline_extractor = HeadlinesExtractor(
+            llm=llm, filter_nodes=lambda node: filter_doc_with_num_tokens(node)
+        )
         splitter = HeadlineSplitter(min_tokens=500)
         summary_extractor = SummaryExtractor(
             llm=llm, filter_nodes=lambda node: filter_doc_with_num_tokens(node)
         )
-        
-        theme_extractor = ThemesExtractor(llm=llm, filter_nodes=lambda node: filter_docs(node))
+
+        theme_extractor = ThemesExtractor(
+            llm=llm, filter_nodes=lambda node: filter_docs(node)
+        )
         ner_extractor = NERExtractor(
             llm=llm, filter_nodes=lambda node: filter_chunks(node)
         )
-        
+
         summary_emb_extractor = EmbeddingExtractor(
             embedding_model=embedding_model,
             property_name="summary_embedding",
@@ -104,13 +111,14 @@ def default_transforms(
             threshold=0.7,
             filter_nodes=lambda node: filter_doc_with_num_tokens(node),
         )
-        
 
         ner_overlap_sim = OverlapScoreBuilder(
             threshold=0.01, filter_nodes=lambda node: filter_chunks(node)
         )
-        
-        node_filter = CustomNodeFilter(llm=llm, filter_nodes=lambda node: filter_chunks(node))
+
+        node_filter = CustomNodeFilter(
+            llm=llm, filter_nodes=lambda node: filter_chunks(node)
+        )
 
         transforms = [
             headline_extractor,
@@ -120,13 +128,37 @@ def default_transforms(
             Parallel(summary_emb_extractor, theme_extractor, ner_extractor),
             Parallel(cosine_sim_builder, ner_overlap_sim),
         ]
-    else:
-        ner_extractor = NERExtractor(
-            llm=llm, filter_nodes=lambda node: filter_chunks(node)
+    elif result["101-500"] >= 0.5:
+        summary_extractor = SummaryExtractor(
+            llm=llm, filter_nodes=lambda node: filter_doc_with_num_tokens(node, 100)
         )
-        ner_overlap_sim = OverlapScoreBuilder(
-            threshold=0.01, filter_nodes=lambda node: filter_chunks(node)
+        summary_emb_extractor = EmbeddingExtractor(
+            embedding_model=embedding_model,
+            property_name="summary_embedding",
+            embed_property_name="summary",
+            filter_nodes=lambda node: filter_doc_with_num_tokens(node, 100),
+        )
+
+        cosine_sim_builder = CosineSimilarityBuilder(
+            property_name="summary_embedding",
+            new_property_name="summary_similarity",
+            threshold=0.7,
+            filter_nodes=lambda node: filter_doc_with_num_tokens(node, 100),
         )
         
+        ner_extractor = NERExtractor(llm=llm)
+        ner_overlap_sim = OverlapScoreBuilder(threshold=0.01)
+        theme_extractor = ThemesExtractor(
+            llm=llm, filter_nodes=lambda node: filter_docs(node)
+        )
+        node_filter = CustomNodeFilter(
+            llm=llm
+        )
+        
+        transforms = [
+            summary_extractor,
+            node_filter,
+            Parallel(summary_emb_extractor, theme_extractor, ner_extractor),
+            ner_overlap_sim]
 
     return transforms
