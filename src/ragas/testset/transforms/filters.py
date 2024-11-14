74dbf1762f6bb246bd4076751dcce70bf565119b
Shahules786 <Shahules786@gmail.com>
2024-11-14 13:42:06 +0530

    modify custom filter

--- src/ragas/testset/transforms/filters.py
+++ src/ragas/testset/transforms/filters.py
@@ -62,15 +62,21 @@ class CustomNodeFilter(LLMBasedNodeFilter):
     rubrics: t.Dict[str, str] = field(default_factory=lambda: DEFAULT_RUBRICS)
 
     async def custom_filter(self, node: Node, kg: KnowledgeGraph) -> bool:
-
-        parent_nodes = get_parent_nodes(node, kg)
-        if len(parent_nodes) > 0:
-            summary = parent_nodes[0].properties.get("summary", "")
+        
+        if node.type.name == "CHUNK":
+            parent_nodes = get_parent_nodes(node, kg)
+            if len(parent_nodes) > 0:
+                summary = parent_nodes[0].properties.get("summary", "")
+            else:
+                summary = ""
         else:
-            summary = ""
-
+            summary = node.properties.get("summary", "")
+            
         if summary == "":
-            logger.warning(f"Node {node} has no parent node with a summary.")
+            logger.warning(
+                f"Node {node.id} does not have a summary. Skipping filtering."
+            )
+            return False
 
         prompt_input = QuestionPotentialInput(
             document_summary=summary,
