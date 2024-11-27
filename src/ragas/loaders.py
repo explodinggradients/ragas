8f65e4784089b3ab02d2976b28ebdb800f0dce69
Shahules786 <Shahules786@gmail.com>
2024-11-27 19:53:05 +0530

    add filter and load

--- src/ragas/loaders.py
+++ src/ragas/loaders.py
@@ -11,6 +11,9 @@ class PromptAnnotation(BaseModel):
     is_accepted: bool
     edited_output: t.Union[t.Dict[str, t.Any], None]
 
+    def __getitem__(self, key):
+        return getattr(self, key)
+
 
 class SampleAnnotation(BaseModel):
     metric_input: t.Dict[str, t.Any]
@@ -18,21 +21,46 @@ class SampleAnnotation(BaseModel):
     prompts: t.Dict[str, PromptAnnotation]
     is_accepted: bool
 
+    def __getitem__(self, key):
+        return getattr(self, key)
+
 
 class MetricAnnotation(BaseModel):
 
     root: t.Dict[str, t.List[SampleAnnotation]]
 
+    def __getitem__(self, key):
+        return self.root[key]
+
     @classmethod
-    def from_json(cls, path) -> "MetricAnnotation":
+    def from_json(cls, path, metric_name: t.Optional[str]) -> "MetricAnnotation":
 
         dataset = json.load(open(path))
+        if metric_name is not None and metric_name not in dataset:
+            raise ValueError(f"Split {metric_name} not found in the dataset.")
+
         return cls(
             root={
                 key: [SampleAnnotation(**sample) for sample in value]
                 for key, value in dataset.items()
+                if metric_name is None or key == metric_name
             }
         )
+
+    def filter(self, function: t.Optional[t.Callable] = None):
+
+        if function is None:
+            function = lambda x: True  # noqa: E731
+
+        return MetricAnnotation(
+            root={
+                key: [sample for sample in value if function(sample)]
+                for key, value in self.root.items()
+            }
+        )
+
+    def __len__(self):
+        return sum(len(value) for value in self.root.values())
 
     def train_test_split(
         self,
@@ -50,12 +78,17 @@ class MetricAnnotation(BaseModel):
         """
         pass
 
-    def batch(self, batch_size: int, stratiy: t.Optional[str] = None):
+    def batch(
+        self,
+        batch_size: int,
+        stratify: t.Optional[str] = None,
+        drop_last_batch: bool = False,
+    ):
         """
         Create a batch iterator.
 
         Parameters:
             batch_size (int): The number of samples in each batch.
             stratify (str): The column to stratify the batches on.
+            drop_last_batch (bool): Whether to drop the last batch if it is smaller than the specified batch size.
         """
-        pass
