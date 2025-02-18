eacebca74a9a78db3601f309abacb3be8dd1f7bc
sahusiddharth <siddharth.sahu@plaksha.edu.in>
2025-02-18 20:43:34 +0530

    added test for embedder

--- tests/unit/test_import.py
+++ tests/unit/test_import.py
@@ -22,6 +22,25 @@ def test_missing_haystack_llmwrapper(monkeypatch):
         HaystackLLMWrapper(haystack_generator=None)
 
 
+def test_missing_haystack_embeddingswrapper(monkeypatch):
+    # Save a reference to the real built-in import
+    real_import = builtins.__import__
+
+    def mocked_import(name, *args, **kwargs):
+        # If we're trying to import "haystack", simulate it's missing
+        if name.startswith("haystack"):
+            raise ImportError("No module named 'haystack'")
+        return real_import(name, *args, **kwargs)
+
+    # Replace the built-in import function with our mock
+    monkeypatch.setattr(builtins, "__import__", mocked_import)
+
+    with pytest.raises(ImportError, match="Haystack is not installed"):
+        from ragas.embeddings import HaystackEmbeddingsWrapper
+
+        HaystackEmbeddingsWrapper(embedder=None)
+
+
 def test_import_module():
     import ragas.metrics
     import ragas.metrics._aspect_critic
