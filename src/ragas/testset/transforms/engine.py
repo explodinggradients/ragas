0d6128492e92f8cee14fdd7f7975f2d4f54ecf80
ahgraber <ahgraber@ninerealmlabs.com>
2025-06-09 12:12:31 -0400

    feat: update engine's apply_transformations to recursively process nested Transformation or Parallel procedures
    - The apply_transforms() function has been updated to handle different types of transformations recursively:
      If transforms is a list, it recursively applies each transform in the list.
      If transforms is a Parallel instance, it recursively applies the transformations contained within the Parallel object.
      If transforms is a BaseGraphTransformation, it generates an execution plan (a list of coroutines), gets a description, and then runs the coroutines asynchronously using run_async_tasks().
      If transforms is none of the above, it raises a ValueError indicating an invalid type.
    - Move apply_nest_asyncio to async_utils for better organization
    - Updated the Parallel class to support transformations with improved type hints.
    - Added unit tests

--- src/ragas/testset/transforms/engine.py
+++ src/ragas/testset/transforms/engine.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-import asyncio
 import logging
 import typing as t
 
 from tqdm.auto import tqdm
 
-from ragas.async_utils import as_completed, run_async_tasks
+from ragas.async_utils import apply_nest_asyncio, as_completed, run_async_tasks
 from ragas.run_config import RunConfig
 from ragas.testset.graph import KnowledgeGraph
 from ragas.testset.transforms.base import BaseGraphTransformation
@@ -17,7 +16,7 @@ if t.TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 Transforms = t.Union[
-    t.List[BaseGraphTransformation],
+    t.List[t.Union[BaseGraphTransformation, "Parallel"]],
     "Parallel",
     BaseGraphTransformation,
 ]
@@ -32,22 +31,28 @@ class Parallel:
     >>> Parallel(HeadlinesExtractor(), SummaryExtractor())
     """
 
-    def __init__(self, *transformations: BaseGraphTransformation):
+    def __init__(self, *transformations: t.Union[BaseGraphTransformation, "Parallel"]):
         self.transformations = list(transformations)
 
-    def generate_execution_plan(self, kg: KnowledgeGraph) -> t.List[t.Coroutine]:
+    def generate_execution_plan(self, kg: KnowledgeGraph) -> t.Sequence[t.Coroutine]:
         coroutines = []
         for transformation in self.transformations:
             coroutines.extend(transformation.generate_execution_plan(kg))
+        class_names = [t.__class__.__name__ for t in self.transformations]
+        logger.debug(
+            f"Created {len(coroutines)} coroutines for transformations: {class_names}"
+        )
         return coroutines
 
 
-async def run_coroutines(coroutines: t.List[t.Coroutine], desc: str, max_workers: int):
+async def run_coroutines(
+    coroutines: t.Sequence[t.Coroutine], desc: str, max_workers: int
+):
     """
     Run a list of coroutines in parallel.
     """
     for future in tqdm(
-        await as_completed(coroutines, max_workers=max_workers),
+        as_completed(coroutines, max_workers=max_workers),
         desc=desc,
         total=len(coroutines),
         # whether you want to keep the progress bar after completion
@@ -67,22 +72,6 @@ def get_desc(transform: BaseGraphTransformation | Parallel):
         return f"Applying {transform.__class__.__name__}"
 
 
-def apply_nest_asyncio():
-    NEST_ASYNCIO_APPLIED: bool = False
-    if is_event_loop_running():
-        # an event loop is running so call nested_asyncio to fix this
-        try:
-            import nest_asyncio
-        except ImportError:
-            raise ImportError(
-                "It seems like your running this in a jupyter-like environment. Please install nest_asyncio with `pip install nest_asyncio` to make it work."
-            )
-
-        if not NEST_ASYNCIO_APPLIED:
-            nest_asyncio.apply()
-            NEST_ASYNCIO_APPLIED = True
-
-
 def apply_transforms(
     kg: KnowledgeGraph,
     transforms: Transforms,
@@ -90,39 +79,36 @@ def apply_transforms(
     callbacks: t.Optional[Callbacks] = None,
 ):
     """
-    Apply a list of transformations to a knowledge graph in place.
+    Recursively apply transformations to a knowledge graph in place.
     """
     # apply nest_asyncio to fix the event loop issue in jupyter
     apply_nest_asyncio()
 
-    # if single transformation, wrap it in a list
-    if isinstance(transforms, BaseGraphTransformation):
-        transforms = [transforms]
+    max_workers = getattr(run_config, "max_workers", -1)
 
-    # apply the transformations
-    # if Sequences, apply each transformation sequentially
-    if isinstance(transforms, t.List):
+    if isinstance(transforms, list):
         for transform in transforms:
-            asyncio.run(
-                run_coroutines(
-                    transform.generate_execution_plan(kg),
-                    get_desc(transform),
-                    run_config.max_workers,
-                )
-            )
-    # if Parallel, collect inside it and run it all
+            apply_transforms(kg, transform, run_config, callbacks)
     elif isinstance(transforms, Parallel):
-        asyncio.run(
-            run_coroutines(
-                transforms.generate_execution_plan(kg),
-                get_desc(transforms),
-                run_config.max_workers,
-            )
+        apply_transforms(kg, transforms.transformations, run_config, callbacks)
+    elif isinstance(transforms, BaseGraphTransformation):
+        logger.debug(
+            f"Generating execution plan for transformation {transforms.__class__.__name__}"
+        )
+        coros = transforms.generate_execution_plan(kg)
+        desc = get_desc(transforms)
+        run_async_tasks(
+            coros,
+            batch_size=None,
+            show_progress=True,
+            progress_bar_desc=desc,
+            max_workers=max_workers,
         )
     else:
         raise ValueError(
             f"Invalid transforms type: {type(transforms)}. Expects a list of BaseGraphTransformations or a Parallel instance."
         )
+    logger.debug("All transformations applied successfully.")
 
 
 def rollback_transforms(kg: KnowledgeGraph, transforms: Transforms):
