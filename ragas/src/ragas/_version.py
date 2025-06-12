65e9b698de1218313e5c1de51b68d5ff51d50748
jjmachan <jamesjithin97@gmail.com>
2025-06-12 10:03:26 -0700

    fixed some ci

--- ragas/src/ragas/_version.py
+++ ragas/src/ragas/_version.py
@@ -5,8 +5,7 @@ __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple
-    from typing import Union
+    from typing import Tuple, Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
@@ -17,5 +16,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.2.16.dev4+g7fd5473.d20250507'
-__version_tuple__ = version_tuple = (0, 2, 16, 'dev4', 'g7fd5473.d20250507')
+__version__ = version = "0.2.16.dev4+g7fd5473.d20250507"
+__version_tuple__ = version_tuple = (0, 2, 16, "dev4", "g7fd5473.d20250507")
