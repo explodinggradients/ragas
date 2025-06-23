1e9b0cffad98a4c7d0989467b434f09d99564ab8
Shahules786 <Shahules786@gmail.com>
2025-06-23 16:08:13 -0700

    add init

--- experimental/ragas_experimental/metric/__init__.py
+++ experimental/ragas_experimental/metric/__init__.py
@@ -1,12 +1,15 @@
 from ragas_experimental.metric.result import MetricResult
 from ragas_experimental.metric.base import Metric
-from ragas_experimental.metric.discrete import DiscreteMetric
-from ragas_experimental.metric.numeric import NumericMetric
-from ragas_experimental.metric.ranking import RankingMetric
+from ragas_experimental.metric.discrete import DiscreteMetric, discrete_metric
+from ragas_experimental.metric.numeric import NumericMetric, numeric_metric
+from ragas_experimental.metric.ranking import RankingMetric, ranking_metric
 
 __all__ = ['MetricResult',
            'Metric',
            'DiscreteMetric',
            'NumericMetric',
            'RankingMetric',
+           'discrete_metric',
+           'numeric_metric',
+           'ranking_metric',
            ]
