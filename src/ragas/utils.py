19e4ff270907100585be016ec4032069d6a536bd
questcollector <miroirs01@gmail.com>
2025-07-04 16:02:35 +0900

    fix: add disallowed_special  on tiktoken encode

--- src/ragas/utils.py
+++ src/ragas/utils.py
@@ -230,7 +230,9 @@ def camel_to_snake(name):
 def num_tokens_from_string(string: str, encoding_name: str = "cl100k_base") -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
-    num_tokens = len(encoding.encode(string))
+    # to prevent error case when document has special tokens like `<endoftext>`
+    # set empty tuple in disallowed_special to allow all special tokens
+    num_tokens = len(encoding.encode(string, disallowed_special=()))
     return num_tokens
 
 
