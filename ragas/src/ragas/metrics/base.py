94e61d291937711487b064d6d453c83cf0df55b0
NirantK <nirant.bits@gmail.com>
2025-08-18 08:08:55 +0530

    refactor: enhance embedding model flexibility by supporting both BaseRagasEmbeddings and BaseRagasEmbedding

--- ragas/src/ragas/metrics/base.py
+++ ragas/src/ragas/metrics/base.py
@@ -24,7 +24,7 @@ if t.TYPE_CHECKING:
     from langchain_core.callbacks import Callbacks
 
     from ragas.config import DemonstrationConfig, InstructionConfig
-    from ragas.embeddings import BaseRagasEmbeddings
+    from ragas.embeddings import BaseRagasEmbeddings, BaseRagasEmbedding
     from ragas.llms import BaseRagasLLM
 
 logger = logging.getLogger(__name__)
@@ -428,7 +428,7 @@ class MetricWithLLM(Metric, PromptMixin):
 
 @dataclass
 class MetricWithEmbeddings(Metric):
-    embeddings: t.Optional[BaseRagasEmbeddings] = None
+    embeddings: t.Optional[t.Union[BaseRagasEmbeddings, BaseRagasEmbedding]] = None
 
     def init(self, run_config: RunConfig):
         if self.embeddings is None:
