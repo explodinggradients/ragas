6549737c5a7d8c1a2824213373c50519e285c9de
Jithin James <jithin@explodinggradients.com>
2025-09-10 16:56:30 -0700

    added rb2b analytics (#2273)

---
+++ docs/_static/js/rb2b.js
@@ -0,0 +1,8 @@
+!function(key) {
+  if (window.reb2b) return;
+  window.reb2b = {loaded: true};
+  var s = document.createElement("script");
+  s.async = true;
+  s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";
+  document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);
+}("0NW1GHLY45O4");
