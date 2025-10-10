cc59f6add80481248acd349b87a672af8260c910
Ani <5357586+anistark@users.noreply.github.com>
2025-10-10 16:22:14 +0530

    feat: bleu score migrated to collections (#2352)

--- src/ragas/metrics/collections/__init__.py
+++ src/ragas/metrics/collections/__init__.py
@@ -1,11 +1,13 @@
 """Collections of metrics using modern component architecture."""
 
 from ragas.metrics.collections._answer_relevancy import AnswerRelevancy
+from ragas.metrics.collections._bleu_score import BleuScore
 from ragas.metrics.collections._rouge_score import RougeScore
 from ragas.metrics.collections.base import BaseMetric
 
 __all__ = [
-    "AnswerRelevancy",  # Class-based answer relevancy
-    "RougeScore",  # Class-based rouge score
-    "BaseMetric",  # Base class for creating new v2 metrics
+    "BaseMetric",  # Base class
+    "AnswerRelevancy",
+    "BleuScore",
+    "RougeScore",
 ]
