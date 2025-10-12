1408f219b8fe453ae9aee85eb1a07a318d2d06df
Kumar Anirudha <mail@anirudha.dev>
2025-10-13 00:40:53 +0530

    feat: answer similarity migrated to collections

--- src/ragas/metrics/collections/__init__.py
+++ src/ragas/metrics/collections/__init__.py
@@ -1,6 +1,7 @@
 """Collections of metrics using modern component architecture."""
 
 from ragas.metrics.collections._answer_relevancy import AnswerRelevancy
+from ragas.metrics.collections._answer_similarity import AnswerSimilarity
 from ragas.metrics.collections._bleu_score import BleuScore
 from ragas.metrics.collections._rouge_score import RougeScore
 from ragas.metrics.collections.base import BaseMetric
@@ -8,6 +9,7 @@ from ragas.metrics.collections.base import BaseMetric
 __all__ = [
     "BaseMetric",  # Base class
     "AnswerRelevancy",
+    "AnswerSimilarity",
     "BleuScore",
     "RougeScore",
 ]
